f138f060b2b95a8344a587711981f631301d1612
Randy Castleman <rcastleman@gmail.com>
2022-07-06 22:47:28 -0400

    name change

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,13 +1,18 @@
-# TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
-# TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
+from ceasar_art import logo
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+#TODO-3: What happens if the user enters a number/symbol/space?
+#Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
+#e.g. start_text = "meet me at 3"
+#end_text = "•••• •• •• 3"
+
+print(logo)
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def ceasar(text,shift,direction):
+def ceasar(start_text, shift_amount, cipher_direction):
     notepad = ""
     for letter in text:
         position = alphabet.index(letter)
@@ -19,7 +24,19 @@ def ceasar(text,shift,direction):
         notepad += new_letter
     print(f"The result is {notepad}")
 
-ceasar(text,shift,direction)
+#TODO-1: Import and print the logo from art.py when the program starts.
+
+#TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
+#Try running the program and entering a shift number of 45.
+#Add some code so that the program continues to work even if the user enters a shift number greater than 26. 
+#Hint: Think about how you can use the modulus (%).
+
+#TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
+#e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
+#If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
+#Hint: Try creating a while loop that continues to execute the program if the user types 'yes'.
+
+ceasar(start_text=text, shift_amount=shift, cipher_direction=direction)
 
 # def encrypt(plain_text, shift_amount):
 #     cipher_text = ""
