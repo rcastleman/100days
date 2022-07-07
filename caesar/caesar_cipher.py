620cdc3b50399e54dad2cf159dc5dd81b1339f86
Randy Castleman <rcastleman@gmail.com>
2022-07-06 22:58:58 -0400

    changed to-dos

--- caesar/caesar_cipher.py
+++ caesar/caesar_cipher.py
@@ -2,11 +2,6 @@ from ceasar_art import logo
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-#TODO-3: What happens if the user enters a number/symbol/space?
-#Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
-#e.g. start_text = "meet me at 3"
-#end_text = "•••• •• •• 3"
-
 print(logo)
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
@@ -24,12 +19,15 @@ def ceasar(start_text, shift_amount, cipher_direction):
         notepad += new_letter
     print(f"The result is {notepad}")
 
-#TODO-1: Import and print the logo from art.py when the program starts.
-
 #TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
 #Try running the program and entering a shift number of 45.
 #Add some code so that the program continues to work even if the user enters a shift number greater than 26. 
 #Hint: Think about how you can use the modulus (%).
+
+#TODO-3: What happens if the user enters a number/symbol/space?
+#Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
+#e.g. start_text = "meet me at 3"
+#end_text = "•••• •• •• 3"
 
 #TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
 #e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
