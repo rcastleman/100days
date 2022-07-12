7e9ab8b0aebebe09288391fca5cfefed7d187ddc
Randy Castleman <rcastleman@gmail.com>
2022-07-11 22:46:52 -0400

    final

--- grading/grading.py
+++ grading/grading.py
@@ -1,3 +1,12 @@
+"""> Scores 91 - 100: Grade = "Outstanding"
+> Scores 81 - 90: Grade = "Exceeds Expectations"
+> Scores 71 - 80: Grade = "Acceptable"
+> Scores 70 or lower: Grade = "Fail"
+# Expected Output
+'{'Harry': 'Exceeds Expectations', 'Ron': 'Acceptable', 'Hermione': 'Outstanding', 'Draco': 'Acceptable', 'Neville': 'Fail'}'
+
+"""
+
 student_scores = {
   "Harry": 81,
   "Ron": 78,
@@ -8,12 +17,18 @@ student_scores = {
 # 🚨 Don't change the code above 👆
 
 #TODO-1: Create an empty dictionary called student_grades.
-
+student_grades = {}
 
 #TODO-2: Write your code below to add the grades to student_grades.👇
-
-    
+for key in student_scores:
+  if student_scores[key] > 90:
+    student_grades[key] = "Outstanding"
+  elif student_scores[key] > 80:
+    student_grades[key] = "Exceeds Expectations"
+  elif student_scores[key] > 70:
+    student_grades[key] = "Acceptable"
+  else:
+    student_grades[key] = "Fail"
 
 # 🚨 Don't change the code below 👇
 print(student_grades)
-
