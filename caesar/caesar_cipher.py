0c3230f66ecd5614dbc816ef44e911ec10dc94b5
Randy Castleman <rcastleman@gmail.com>
2022-07-11 21:07:04 -0400

    final

--- caesar/caesar_cipher.py
+++ caesar/caesar_cipher.py
@@ -1,7 +1,7 @@
 from caesar_art import logo
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-symbol_list = []
+# symbol_list = []
 print(logo)
 
 def caesar(start_text, shift_amount, cipher_direction):
@@ -22,15 +22,15 @@ def caesar(start_text, shift_amount, cipher_direction):
     print(f"The result is '{notepad}'.")
 
 should_end = False
-
 while not should_end:
-    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-    text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number: \n"))
 
-caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
 
-restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
-if restart == "no":
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+  restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+  if restart == "no":
     should_end = True
     print("Goodbye")
