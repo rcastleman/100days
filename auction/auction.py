56297b1ef7819d4efbda33b6a642ed1e5841b2eb
Randy Castleman <rcastleman@gmail.com>
2022-07-14 18:00:53 -0400

    refinements

--- auction/auction.py
+++ auction/auction.py
@@ -7,13 +7,13 @@ def clear():
     else:
         _ = system('clear')
 
-# print(logo)
+print(logo)
 
 def find_winner(bidding_record):
     highest_bid = 0
     winner = ""
     for bidder in bidding_record:
-        bid_amount = int(bidding_record[bidder])
+        bid_amount = bidding_record[bidder]
         if bid_amount > highest_bid:
             highest_bid = bid_amount
             winner = bidder
@@ -25,7 +25,7 @@ bidding_finished = False
 
 while bidding_finished == False:
     name = input("Name: ")
-    price = input("Bid Price: ")
+    price = int(input("Bid Price: "))
     bids[name] = price
     should_continue = input("Any other users to bid (yes or no)?").lower()
     if should_continue == "no":
