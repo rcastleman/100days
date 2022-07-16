94be162e370c89f0ec3bc9384293b8e1b4c200a3
Randy Castleman <rcastleman@gmail.com>
2022-07-16 17:41:54 -0400

    endless calcs

--- calculator/calc.py
+++ calculator/calc.py
@@ -25,11 +25,18 @@ for op in operations:
     op_choice += op + " "
 
 num1 = int(input("What is the first number?: "))
-num2 = int(input("What is the second number?: "))
-op = input(f"Which operation ({op_choice}?")
 
-calculation_function = operations[op]
-answer = calculation_function(num1,num2)
+should_continue = True
 
-# answer = operations["op_choice"]
-print(f"{num1} {op} {num2} = {answer} ")
+while should_continue:
+    
+    op = input(f"Which operation ({op_choice}?")
+    num2 = int(input("What is the next number?: "))
+    calculation_function = operations[op]
+    answer = calculation_function(num1,num2)
+    print(f"{num1} {op} {num2} = {answer} ")
+    if input("Add another operation? (y/n): ").lower() == "y":
+        num1 = answer
+    else:
+        should_continue = False
+
