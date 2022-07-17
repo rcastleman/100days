b5af50a283df441c8229bd008a8a26dde26e2679
Randy Castleman <rcastleman@gmail.com>
2022-07-16 22:25:57 -0400

    final

--- calculator/calc.py
+++ calculator/calc.py
@@ -24,19 +24,22 @@ op_choice = ""
 for op in operations:
     op_choice += op + " "
 
-num1 = int(input("What is the first number?: "))
+def calculator():
+    num1 = float(input("What is the first number?: "))
 
-should_continue = True
+    should_continue = True
 
-while should_continue:
-    
-    op = input(f"Which operation ({op_choice}?")
-    num2 = int(input("What is the next number?: "))
-    calculation_function = operations[op]
-    answer = calculation_function(num1,num2)
-    print(f"{num1} {op} {num2} = {answer} ")
-    if input("Add another operation? (y/n): ").lower() == "y":
-        num1 = answer
-    else:
-        should_continue = False
+    while should_continue:
+        
+        op = input(f"Which operation ({op_choice}?")
+        num2 = float(input("What is the next number?: "))
+        calculation_function = operations[op]
+        answer = calculation_function(num1,num2)
+        print(f"{num1} {op} {num2} = {answer} ")
+        if input(f"Type 'y' to continue calculation with {answer}: ").lower() == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
 
+calculator()
