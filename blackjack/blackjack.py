a17b45e4b481ee3dfceb3225017293f8fdf5c812
Randy Castleman <rcastleman@gmail.com>
2022-07-21 22:47:38 -0400

    final

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -13,7 +13,7 @@ cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 #Angela's code
 
 def play_game():
-    # print(logo)
+    print(logo)
     game_over = False
     def calculate_score(cards):
         """Takes a list of cards and calculates score"""
