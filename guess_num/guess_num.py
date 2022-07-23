62c925f635fcae10c52cc4d714254c444ec3d5de
Randy Castleman <rcastleman@gmail.com>
2022-07-22 22:09:11 -0400

    no while loop needed

--- guess_num/guess_num.py
+++ guess_num/guess_num.py
@@ -1,4 +1,3 @@
-# from art import logo
 import random
 from os import system, name
 
@@ -8,9 +7,6 @@ def clear():
     else:
         _ = system('clear')
 
-# print(logo)
-
-#welcome
 print("Welcome to the Number Guessing Game")
 print("I'm thinking of a number between 1 and 100")
 
@@ -29,18 +25,13 @@ print(f"(the secret numbers is {num})")
 print(f"You have {tries_left} tries left")
 guess = int(input("Make a guess:  "))
 
-# game_over = False
-
-# while game_over == False:
 def evaluate_guess():
     global tries_left, guess, game_over
-    if tries_left < 1:
+    if tries_left < 2:
         print("Game Over! You ran out of tries.")
-        # game_over = True
         return 
     if guess == num:
         print(f"You guessed the number ({num})!")
-        # game_over = True
         return
     elif guess < num:
         tries_left -= 1
@@ -52,7 +43,3 @@ def evaluate_guess():
         evaluate_guess()
 
 evaluate_guess()
-
-# while input("Do you want to play again? Type 'y' or 'n'") == "y":
-#     clear()
-#     play_game()
