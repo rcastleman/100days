d64077fcb488c791889175f739d9ac9cf9767198
Randy Castleman <rcastleman@gmail.com>
2022-07-26 22:07:12 -0400

    3rd commit

--- coffee_machine/coffee_machine.py
+++ coffee_machine/coffee_machine.py
@@ -1,9 +1,28 @@
+from menu import MENU
 
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+    "money": 0}
 
+def format_report(resources):
+    return f"Water: {resources['water']}"
+    return f"Milk: {resources['milk']}"
+    return f"Coffee: {resources['coffee']}"
+    return f"Money: {resources['money']}"
 
-# TODO Prompt user by asking “ What would you like? (espresso/latte/cappuccino): ”
+print(format_report(resources=))
+
 # TODO Turn off the Coffee Machine by entering “ off ” to the prompt.
+"""For maintainers of the coffee machine, they can use 'off' as the secret word to turn off
+the machine. Your code should end execution when this happens."""
+# flag = True
+# while flag: 
+
 # TODO Print report.
+
+
 # Water: 100ml
 # Milk: 50ml
 # Coffee: 76g
@@ -13,9 +32,5 @@
 # TODO transaction successful? Give change Give coffee
 # TODO “Here is your {coffee choice}. Enjoy!”.
 
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
-
+# Prompt user by asking “ What would you like? (espresso/latte/cappuccino): ”
+# choice = input("What would you like? (espresso/latte/cappuccino): ")
