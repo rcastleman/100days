ba16c27adbcfcdd461d0f142fd657efb00783013
Randy Castleman <rcastleman@gmail.com>
2022-07-26 21:27:46 -0400

    2nd commit

--- coffee_machine/coffee_machine.py
+++ coffee_machine/coffee_machine.py
@@ -1,28 +1,17 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
+
+
+
+# TODO Prompt user by asking “ What would you like? (espresso/latte/cappuccino): ”
+# TODO Turn off the Coffee Machine by entering “ off ” to the prompt.
+# TODO Print report.
+# Water: 100ml
+# Milk: 50ml
+# Coffee: 76g
+# Money: $2.5
+# TODO Check resources sufficient?
+# TODO If sufficient resources, process coins
+# TODO transaction successful? Give change Give coffee
+# TODO “Here is your {coffee choice}. Enjoy!”.
 
 resources = {
     "water": 300,
