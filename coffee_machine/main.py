ad8af8e0296e73b161da8edefe842b391f3c0a33
Randy Castleman <rcastleman@gmail.com>
2022-07-28 15:19:55 -0400

    working?...

--- coffee_machine/main.py
+++ coffee_machine/main.py
@@ -35,16 +35,18 @@ money_dict = {
     "pennies":0.01}
 
 def cashier(money_dict,choice):
+    #ask for money
     print("Please insert coins.")
     quarters = float(input("How many quarters?:  "))
     dimes = float(input("How many dimes?:  "))
     nickels = float(input("How many nickels?:  "))
     pennies = float(input("How many pennies?:  "))
+    #calculate total paid
     paid = money_dict["quarters"]*quarters + money_dict["dimes"]*dimes + money_dict["nickels"]*nickels + money_dict["pennies"]*pennies
     f_paid = "${:.2f}".format(paid) #formatted total
     print(f"You paid: {f_paid}")
     resources["money"] += paid
-
+    #determine if amount paid is sufficient
     price = price_list[choice]
     f_price = "${:.2f}".format(price)
     print(f"The price for your {choice} is {f_price}")
@@ -68,41 +70,39 @@ def check_resources(resources,resource_requirements,choice):
     global sufficient_resources
     if resource_requirements[choice]["water"] > resources["water"]:
         sufficient_resources = False
-        print(f"Sorry, there's not enough water for a {choice}")
+        print(f"Sorry, there's not enough water for your {choice}")
     elif resource_requirements[choice]["milk"] > resources["milk"]:
         sufficient_resources = False
-        print(f"Sorry, there's not enough milk for a {choice}")
+        print(f"Sorry, there's not enough milk for your {choice}")
     elif resource_requirements[choice]["coffee"] > resources["coffee"]:
         sufficient_resources = False
-        print(f"Sorry, there's not enough coffee for a {choice}")
+        print(f"Sorry, there's not enough coffee for your {choice}")
     else:
+        print(f"Good news, resources are sufficient to make your {choice}!")
         sufficient_resources = True
+    return sufficient_resources
 
-# check_resources(resources,resource_requirements,"cappuccino")
-
-choice = 'latte'
-print(resource_requirements[choice]["water"], "vs", resources["water"])
-print(resource_requirements[choice]["milk"] > resources["milk"])
-print(resource_requirements[choice]["coffee"] > resources["coffee"])
+def decrement_resources(choice):
+    resources["water"] -= resource_requirements[choice]["water"]
+    resources["milk"] -= resource_requirements[choice]["milk"]
+    resources["coffee"] -= resource_requirements[choice]["coffee"]
 
 while not off:
 
     def barrista():
         global sufficient_resources, enough_money, off
-        choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
+        choice = input("\r\nWelcome to the Coffee Bar \r\nWhat would you like? (espresso/latte/cappuccino): ").lower()
         if choice == 'off':
             print("Okay, shutting down ... ")
             off = True
         elif choice == 'report':
             print(format_report(resources))
         elif choice in MENU.keys():
-            if sufficient_resources == True:
+            if check_resources(resources,resource_requirements,choice):
                 cashier(money_dict,choice)
+                decrement_resources(choice)
         else: 
             print("Sorry, that's not on the menu.  Please choose an espresso, latte, or cappuccino.")
         return choice
 
-    # barrista()
-
-#TODO decrement resources
-#TODO increment / decrement
+    barrista()
