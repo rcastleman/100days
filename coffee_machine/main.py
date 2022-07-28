ac5f23a7cd42a73722a000ef7ee80c9d8edbb579
Randy Castleman <rcastleman@gmail.com>
2022-07-28 15:23:36 -0400

    fully functional?

--- coffee_machine/main.py
+++ coffee_machine/main.py
@@ -91,12 +91,16 @@ while not off:
 
     def barrista():
         global sufficient_resources, enough_money, off
+        #ask user choice
         choice = input("\r\nWelcome to the Coffee Bar \r\nWhat would you like? (espresso/latte/cappuccino): ").lower()
+        #shut down if 'off' 
         if choice == 'off':
             print("Okay, shutting down ... ")
             off = True
+        #generate report if 'report'
         elif choice == 'report':
             print(format_report(resources))
+        #if choice is legit, ask for $ and decrement resources
         elif choice in MENU.keys():
             if check_resources(resources,resource_requirements,choice):
                 cashier(money_dict,choice)
