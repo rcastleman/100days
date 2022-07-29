65338a4eb90fd7e651a88f9da1f58c3a05c5a263
Randy Castleman <rcastleman@gmail.com>
2022-07-28 22:08:34 -0400

    fun with objects...

--- coffee_OOP/main.py
+++ coffee_OOP/main.py
@@ -2,23 +2,33 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+machine = CoffeeMaker()
+cashier = MoneyMachine()
+menu = Menu()
 
-def barrista():
-        global sufficient_resources, enough_money, off
-        #ask user choice
-        choice = input("\r\nWelcome to the Coffee Bar \r\nWhat would you like? (espresso/latte/cappuccino): ").lower()
-        #shut down if 'off' 
-        if choice == 'off':
-            print("Okay, shutting down ... ")
-            off = True
-        #generate report if 'report'
-        elif choice == 'report':
-            print(format_report(resources))
-        #if choice is legit, ask for $ and decrement resources
-        elif choice in MENU.keys():
-            if check_resources(resources,resource_requirements,choice):
-                cashier(money_dict,choice)
-                decrement_resources(choice)
-        else: 
-            print("Sorry, that's not on the menu.  Please choose an espresso, latte, or cappuccino.")
-        return choice
+machine_on = True
+
+while machine_on:
+
+    def barrista():
+            global machine_on
+            #ask user choice
+            choice = input("\r\nWelcome to the Coffee Bar \r\nWhat would you like? (espresso/latte/cappuccino): ").lower()
+            #shut down if 'off' 
+            if choice == 'off':
+                print("Okay, shutting down ... ")
+                machine_on = False
+            #generate report if 'report'
+            elif choice == 'report':
+                CoffeeMaker.report(machine)
+            #is choice in Menu? 
+            elif menu.find_drink(choice) and machine.is_resource_sufficient(choice):
+                #create MenuItem object??
+                # order = MenuItem(ARGS????)
+                #ask for money
+                # cashier.process_coins()
+                # cashier.make_payment(MenuItem)
+                # machine.make_coffee(choice)
+                return choice
+
+    barrista()
