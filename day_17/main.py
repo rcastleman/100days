f5d0038b00e0c0209cf2a27230351634a5a08f0e
Randy Castleman <rcastleman@gmail.com>
2022-07-29 23:34:57 -0400

    initial class work

---
+++ day_17/main.py
@@ -0,0 +1,14 @@
+class User:
+
+    def __init__(self,user_id,username):
+        self.id = user_id
+        self.username = username
+
+
+user_1 = User("001","angela")
+
+print(user_1.id)
+
+# user2 = User()
+# user2.id = "002"
+# user2.name = "jack"
