18cd10a5a0172830f39314ffbc74bbe85e7cba77
Randy Castleman <rcastleman@gmail.com>
2022-07-30 16:29:58 -0400

    progress on Quiz

--- quiz_game/main.py
+++ quiz_game/main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
@@ -7,5 +8,10 @@ for i in range (len(question_data)):
     question = Question(question_data[i]["text"],question_data[i]["answer"])
     question_bank.append(question)
 
-for element in question_bank:
-    print(element.__dict__)
+# for element in question_bank:
+#     print(element.__dict__)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
--- quiz_game/question_model.py
+++ quiz_game/question_model.py
@@ -1,7 +1,5 @@
 class Question:
 
-    def __init__(self,question,answer):
-        self.question = question
-        self.answer = answer
-
-
+    def __init__(self,q_text,q_answer):
+        self.text = q_text
+        self.answer = q_answer
