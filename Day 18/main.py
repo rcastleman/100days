99b1e5adb09426ff7e62773bf8aaf3132645ed5d
Randy Castleman <rcastleman@gmail.com>
2022-08-01 21:54:36 -0400

    colors

--- Day 18/main.py
+++ Day 18/main.py
@@ -1,12 +1,27 @@
-from turtle import Turtle,Screen
+from turtle import Turtle,Screen, color, pencolor
+import random
 
 screen = Screen()
-
 timmy = Turtle()
 timmy.shape("turtle")
-timmy.color("red")
-for _ in range(4):
-    timmy.forward(100)
-    timmy.right(90)
+# timmy.color("blue")
+# 
+colors = ["gray","blue","red","green","black","brown","orange","chartreuse","hot pink","dark violet"]
+
+def draw_shape(sides,angle):
+    for _ in range(sides):
+        timmy.forward(100)
+        timmy.right(angle)
+
+for sides in range(3,11):
+    angle = 360 / sides
+    timmy.color(random.choice(colors))
+    draw_shape(sides,angle)
+
+# for _ in range(10):
+#     timmy.pendown()
+#     timmy.forward(10)
+#     timmy.penup()
+#     timmy.forward(10)
 
 screen.exitonclick()
