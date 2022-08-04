08465b76fcd8222d549e92e3b25ead2b1665e785
Randy Castleman <rcastleman@gmail.com>
2022-08-03 21:33:56 -0400

    looping

--- day_19/day19.py
+++ day_19/day19.py
@@ -1,11 +1,13 @@
 from turtle import Turtle,Screen
-
-from numpy import spacing
+import random 
 
 screen = Screen()
 screen.setup(width=500,height=400)
 # user_bet = screen.textinput(title="Make your bet",prompt="Which turtle? enter a color: ")
 colors = ["red","orange","yellow","green","blue","purple"]
+
+racers = []
+
 
 def create_turtles():
     x = -230
@@ -16,13 +18,22 @@ def create_turtles():
         racer.color(color)
         racer.penup()
         racer.setpos(x,y)
+        racers.append(racer)
         y += spacing
 
+def advance(racer):
+    amount = random.randint(0,25)
+    racer.forward(amount)
+
+
+race_not_over = True
+while race_not_over:
+    for racer in racers:
+        advance(racer)
+        if racer.xcor == 250:
+            print(f"Race over!  {racer} is the winner")
+            race_not_over = False
+
 create_turtles()
-
-# tim = Turtle(shape="turtle")
-# tim.penup()
-# tim.goto(x=-230,y =-100)
-
 
 screen.exitonclick()
