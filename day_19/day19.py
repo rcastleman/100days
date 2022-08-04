58e64cf14a92c8306a2a5dff9af6e4b3d38e18ac
Randy Castleman <rcastleman@gmail.com>
2022-08-03 20:41:16 -0400

    etch a sketch

---
+++ day_19/day19.py
@@ -0,0 +1,33 @@
+from turtle import Turtle,Screen
+
+tim = Turtle()
+screen = Screen()
+
+def move_forward():
+    tim.forward(10)
+
+def move_back():
+    tim.back(10)
+
+def move_right():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+
+def move_left():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+screen.listen()
+screen.onkey(key = "w",fun=move_forward)
+screen.onkey(key = "s",fun=move_back)
+screen.onkey(key = "d",fun=move_right)
+screen.onkey(key = "a",fun=move_left)
+screen.onkey(key = "c",fun=clear)
+
+screen.exitonclick()
