a63ade8c636f56a2a7cb968f1421b564dfae4be0
Randy Castleman <rcastleman@gmail.com>
2022-08-05 17:10:07 -0400

    snake game start

--- snake_game/main.py
+++ snake_game/main.py
@@ -4,14 +4,15 @@ screen = Screen()
 screen.setup(width=600,height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+
+x = 0
+y = 0
+spacing = 20
+
+for i in range(3):
+    segment_i = Turtle("square")
+    segment_i.color("white")
+    segment_i.setpos(x,y)
+    x -= spacing
+
 screen.exitonclick()
-
-def create_snake():
-    # for _ in range(3):
-    snake = Turtle(shape="square")
-    snake.color("white")
-    # snake.shapesize(20,20)
-    # snake.xcor -= 10
-
-create_snake()
-    
