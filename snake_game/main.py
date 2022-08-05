f950b339f0d8991d89b58e7d14c77b80f2b7fe25
Randy Castleman <rcastleman@gmail.com>
2022-08-05 17:26:40 -0400

    snake game

--- snake_game/main.py
+++ snake_game/main.py
@@ -9,10 +9,20 @@ x = 0
 y = 0
 spacing = 20
 
+segments = []
+
 for i in range(3):
     segment_i = Turtle("square")
+    segment_i.penup()
     segment_i.color("white")
     segment_i.setpos(x,y)
+    segments.append(segment_i)
     x -= spacing
+
+game_is_on = True
+while game_is_on:
+    for seg in segments:
+        seg.penup()
+        seg.forward(20)
 
 screen.exitonclick()
