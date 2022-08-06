af68d55885c2b2242deab487cde26d6cbaf2a92f
Randy Castleman <rcastleman@gmail.com>
2022-08-06 19:31:53 -0400

    snake game

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,9 +1,11 @@
 from turtle import Screen,Turtle
+import time
 
 screen = Screen()
 screen.setup(width=600,height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+# screen.tracer(0)
 
 x = 0
 y = 0
@@ -19,10 +21,13 @@ for i in range(3):
     segments.append(segment_i)
     x -= spacing
 
+# screen.update()
+
 game_is_on = True
 while game_is_on:
+    screen.update()
+    time.sleep(0.1)
     for seg in segments:
-        seg.penup()
         seg.forward(20)
 
 screen.exitonclick()
