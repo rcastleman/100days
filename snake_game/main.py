a931da45c58345c9e5e4acc2366c2b0ada6e06ce
Randy Castleman <rcastleman@gmail.com>
2022-08-06 21:28:06 -0400

    snake game with modules

--- snake_game/main.py
+++ snake_game/main.py
@@ -2,13 +2,14 @@ from turtle import Screen,Turtle
 from snake import Snake
 import time
 
+#screen setup
 screen = Screen()
 screen.setup(width=600,height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-snake.create_snake()
+snake = Snake()
 
 game_is_on = True
 while game_is_on:
@@ -16,4 +17,4 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
-screen.exitonclick()
+# screen.exitonclick()
--- snake_game/snake.py
+++ snake_game/snake.py
@@ -1,15 +1,21 @@
 from turtle import Screen,Turtle
 import time
 
+MOVE_DISTANCE = 20
+INITIAL_NUM_SEGMENTS = 3
+
 class Snake:
 
-    def __init__(self,num_segments):
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        # self.num_segments = num_segments
+
+    def create_snake(self):
         self.x = 0
         self.y = 0
         self.spacing = 20
-        self.num_segments = num_segments
-        self.segments = []
-        for i in range(self.num_segments):
+        for i in range(INITIAL_NUM_SEGMENTS):
             self.segment_i = Turtle("square")
             self.segment_i.color("white")
             self.segment_i.penup()
@@ -17,11 +23,11 @@ class Snake:
             self.segments.append(self.segment_i)
             self.x -= self.spacing
 
-    def move():
-            """loops through all segments (no matter how many) starting from the LAST ... each segment goes to the position of the segment before it (ie last, next-to-last, etc.) thus following the first segment, no matter where it goes"""
-            for segnum in range(len(snake.segments)-1,0,-1): 
-                new_x = segments[segnum - 1].xcor()
-                new_y = segments[segnum - 1].ycor()
-                segments[segnum].goto(new_x,new_y)
-            segments[0].forward(20)
-            segments[0].left(90)
+    def move(self):
+            """loops through any number of segments from the LAST segment to the first, moving each to the position of the segment in front of it, thus *following* the first segment"""
+            for segnum in range(len(self.segments)-1,0,-1): 
+                new_x = self.segments[segnum - 1].xcor()
+                new_y = self.segments[segnum - 1].ycor()
+                self.segments[segnum].goto(new_x,new_y)
+            self.segments[0].forward(MOVE_DISTANCE)
+            # self.segments[0].left(90)
