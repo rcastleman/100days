48905b2dcf48723971745a95fed7210d6c3b697c
Randy Castleman <rcastleman@gmail.com>
2022-08-07 22:52:19 -0400

    screen tracer and screen update

---
+++ pong/paddle.py
@@ -0,0 +1,20 @@
+from mimetypes import init
+from turtle import Turtle
+
+class Paddle:
+    
+    def __init__(self) -> None:
+        pass
+        
+    def create_paddle(self):
+        self.xcor = 350
+        self.ycor = 0
+        self.width = 20
+        self.height = 100
+    
+    def paddle_up():
+        pass
+
+    def paddle_down():
+        pass
+
