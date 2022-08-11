369801d047581710eb561fe89b4aed3c1f8e696a
Randy Castleman <rcastleman@gmail.com>
2022-08-11 15:40:16 +0200

    paddles working

--- pong/paddle.py
+++ pong/paddle.py
@@ -1,21 +1,19 @@
 
 from turtle import Turtle
 
-class Paddle:
+class Paddle(Turtle):
     
-    def __init__(self) -> None:
-        pass
-        
-    def create_paddle(self,x_cor,y_cor):
-        self.xcor = x_cor
-        self.ycor = y_cor
-        self.width = 20
-        self.height = 100
+    def __init__(self,position) -> None:
+        super().__init__()
+        # self.xcor = x_cor
+        # self.ycor = y_cor
+        # self.width = 20
+        # self.height = 100
         self.shape("square")
         self.color("white")
         self.shapesize(stretch_wid=5,stretch_len=1)
         self.penup()
-        self.goto(x_cor,y_cor)
+        self.goto(position)
     
     def go_up(self):
         new_y = self.ycor() + 20
@@ -24,7 +22,3 @@ class Paddle:
     def go_down(self):
         new_y = self.ycor() - 20
         self.goto(self.xcor(),new_y)
-
-    screen.listen()
-    screen.onkey(go_up,"Up")
-    screen.onkey(go_down,"Down")
