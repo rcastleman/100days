268153d4f9e069fd5b475cfb0edc5be3a5c78e1b
Randy Castleman <rcastleman@gmail.com>
2022-08-11 13:38:28 +0300

    added Paddle class

--- pong/paddle.py
+++ pong/paddle.py
@@ -1,4 +1,4 @@
-from mimetypes import init
+
 from turtle import Turtle
 
 class Paddle:
@@ -6,15 +6,25 @@ class Paddle:
     def __init__(self) -> None:
         pass
         
-    def create_paddle(self):
-        self.xcor = 350
-        self.ycor = 0
+    def create_paddle(self,x_cor,y_cor):
+        self.xcor = x_cor
+        self.ycor = y_cor
         self.width = 20
         self.height = 100
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5,stretch_len=1)
+        self.penup()
+        self.goto(x_cor,y_cor)
     
-    def paddle_up():
-        pass
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(),new_y)
 
-    def paddle_down():
-        pass
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(),new_y)
 
+    screen.listen()
+    screen.onkey(go_up,"Up")
+    screen.onkey(go_down,"Down")
