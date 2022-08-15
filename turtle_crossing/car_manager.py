df38fb1fcf60fa28041198ff13a8001cacfc5bc3
Randy Castleman <rcastleman@gmail.com>
2022-08-14 20:36:29 -0400

    added Car class

--- turtle_crossing/car_manager.py
+++ turtle_crossing/car_manager.py
@@ -6,11 +6,16 @@ COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
+last_position = (280,0)
 
 class CarManager(Turtle):
 
-    def __init__(self):
+    def __init__(self,last_position):
         super().__init__()
         self.shape("square")
         self.shapesize(1,2)
+        self.penup()
         self.color(random.choice(COLORS))
+        #initializes car at x = 280 and a random y outside of the safe zones 
+        postition = self.goto(280,random.randint(-250,250))
+        last_position = postition
