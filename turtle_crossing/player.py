63e88e0e6c457c8fc1c92c6464e8625e4c885079
Randy Castleman <rcastleman@gmail.com>
2022-08-14 20:14:46 -0400

    turtle crossing added Player class

--- turtle_crossing/player.py
+++ turtle_crossing/player.py
@@ -1,7 +1,23 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
+class Player(Turtle):
+    
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.color("purple")
+        self.penup()
+        self.setheading(90)
+        self.goto(0,-270)
+    
+    def go_up(self):
+        new_y = self.ycor() + MOVE_DISTANCE
+        self.goto(self.xcor(),new_y)
 
-class Player:
-    pass
+    def go_down(self):
+        new_y = self.ycor() - MOVE_DISTANCE
+        self.goto(self.xcor(),new_y)
