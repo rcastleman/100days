bd1bd35b17a69c99b6f468b37264c563c375ded8
Randy Castleman <rcastleman@gmail.com>
2022-08-16 20:33:48 -0400

    Snake Game - add High Score

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -10,13 +10,19 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(0,270)
         self.hideturtle()
+        self.high_score = 0
     
     def update_scoreboard(self):
         self.write(f"Score: {self.score}",align =ALIGNMENT,font = FONT)
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER",align = ALIGNMENT,font = FONT)
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("GAME OVER",align = ALIGNMENT,font = FONT)
+
+    def reset(self) -> None:
+        if self.score > self.high_score:
+            self.high_score = self.score
+            self.score = 0
 
     def increase_score(self):
         self.score += 1
