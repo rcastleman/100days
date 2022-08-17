684623b205837b6acc691e386eac2b98cc2c2929
Randy Castleman <rcastleman@gmail.com>
2022-08-16 21:10:02 -0400

    file read, write, creation

--- day_24/main.py
+++ day_24/main.py
@@ -3,7 +3,19 @@
 # print(f"\nThe contents are: '{contents}'")
 # file.close()
 
-#obviates the need to close the file
-with open("my_file.txt") as file:
-    contents = file.read()
-    print(contents)
+#WITH obviates the need to close the file
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+#OVERWRITE file:
+# with open("my_file.txt",mode = 'w') as file:
+#     file.write("some new text here ... ")
+
+
+#APPEND to the file:
+# with open("my_file.txt",mode = 'a') as file:
+#     file.write("\n some new text here ... ")
+
+with open("my_new_file.txt",mode = 'w') as file:
+    file.write("some new tezt in a new file")
