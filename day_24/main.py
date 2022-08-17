1d04f79386dbbed63d24aeffb0003787c794db85
Randy Castleman <rcastleman@gmail.com>
2022-08-16 21:02:38 -0400

    implement with method of file i/o

--- day_24/main.py
+++ day_24/main.py
@@ -1,3 +1,9 @@
-file = open("my_file.txt")
-contents = file.read()
-print(f"\nThe contents are: '{contents}'")
+# file = open("my_file.txt")
+# contents = file.read()
+# print(f"\nThe contents are: '{contents}'")
+# file.close()
+
+#obviates the need to close the file
+with open("my_file.txt") as file:
+    contents = file.read()
+    print(contents)
