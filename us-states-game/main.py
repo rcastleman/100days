9e25386248b6ecf954ec14924a3ec6cc6f2241f5
Randy Castleman <rcastleman@gmail.com>
2022-08-18 23:06:12 -0400

    nowhere close...

--- us-states-game/main.py
+++ us-states-game/main.py
@@ -1,4 +1,7 @@
+from re import U
+from socketserver import UnixStreamServer
 import turtle
+import pandas as pd 
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -6,15 +9,33 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-# get all the x,y coordinates of the states
-# def get_mouse_click_coor(x,y):
-#     print(x,y)
-# turtle.onscreenclick(get_mouse_click_coor)
-# turtle.mainloop()
+answer_state = screen.textinput(title = "Guess the State",
+prompt = "What's another state's name?")
 
-answer_state = screen.textinput(title = "Guess the State",prompt = "What's another state's name?")
+user_input = answer_state.title()
 
-#TODO check if answer_state.Title() is in list
+states = pd.read_csv("50_states.csv")
+
+def check_input(user_input):
+    for state in states["state"]:
+        if input == state:
+            print("Y")
+        else:
+            print("N")
+
+print(check_input(user_input))
+
+# print(states["state"])
+
+# if user_input in states["state"]:
+#     print("You got one!")
+# else:
+#     print("Wrong")
+
+#TODO check if answer_state.Title() is in list of states
 #IF YES
-#TODO write state on to screen at x,y value
-#TODO create state counter 
+    #TODO write Guess to screen at x,y value
+#IF NO
+    #ask user to make another guess
+#TODO record correct guesses in a list
+#TODO keep track of score
