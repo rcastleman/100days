d540c9cc38acb905b57704392f76c5195d1db4cf
Randy Castleman <rcastleman@gmail.com>
2022-08-18 20:23:08 -0400

    data ingest

--- weather/main.py
+++ weather/main.py
@@ -1,1 +1,12 @@
+from dataclasses import dataclass
+from email.contentmanager import raw_data_manager
 import pandas as pd
+
+with open("weather_data.csv") as file:
+    raw_data = file.readlines()
+    clean_data = []
+    for e in raw_data:
+        e = e.replace("\n","")
+        clean_data.append(e)
+    
+print(clean_data[1:])
