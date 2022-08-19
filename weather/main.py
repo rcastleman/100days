72a2420a81f610274dc3cf378bcb4ecfe1319908
Randy Castleman <rcastleman@gmail.com>
2022-08-18 20:52:33 -0400

    adding pandas

--- weather/main.py
+++ weather/main.py
@@ -15,8 +15,11 @@ with open ("weather_data.csv") as data_file:
     data = csv.reader(data_file)  
 # print(data)
 #create list of temperatures where values are integers
-    temperatures = []
-    for row in data:
-        if row[1] != "temp":
-            temperatures.append(int(row[1]))
-    print(temperatures)
+    # temperatures = []
+    # for row in data:
+    #     if row[1] != "temp":
+    #         temperatures.append(int(row[1]))
+    # print(temperatures)
+
+data = pd.read_csv("weather_data.csv")
+print(data['temp'])
