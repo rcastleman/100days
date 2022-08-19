8409a709b06990ac0a16ebd27adb3729faff314b
Randy Castleman <rcastleman@gmail.com>
2022-08-18 22:12:12 -0400

    squirrels all the way down...

--- day_24 (pandas)/main.py
+++ day_24 (pandas)/main.py
@@ -10,53 +10,67 @@ from dataclasses import dataclass
 #         clean_data.append(e)
 # print(clean_data[1:])
 
+# with open ("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)  
+# # print(data)
+# #create list of temperatures where values are integers
+#     # temperatures = []
+#     # for row in data:
+#     #     if row[1] != "temp":
+#     #         temperatures.append(int(row[1]))
+#     # print(temperatures)
 
-with open ("weather_data.csv") as data_file:
-    data = csv.reader(data_file)  
-# print(data)
-#create list of temperatures where values are integers
-    # temperatures = []
-    # for row in data:
-    #     if row[1] != "temp":
-    #         temperatures.append(int(row[1]))
-    # print(temperatures)
+# data = pd.read_csv("weather_data.csv")
+# # print(data)
 
-data = pd.read_csv("weather_data.csv")
-# print(data)
+# data_dict = data.to_dict()
+# # print(data_dict)
 
-data_dict = data.to_dict()
-# print(data_dict)
+# temp_list = data["temp"].to_list()
+# # print(temp_list)
+# # average = sum(temp_list)/len(temp_list)
+# # print(average)
+# # max = data["temp"].max()
 
-temp_list = data["temp"].to_list()
-# print(temp_list)
-# average = sum(temp_list)/len(temp_list)
-# print(average)
-# max = data["temp"].max()
+# # print(data.day)
 
-# print(data.day)
+# def convert_CtoF(temp):
+#     return temp * 9/5 + 32
 
-def convert_CtoF(temp):
-    return temp * 9/5 + 32
+# monday = data[data.day == "Monday"]
+# monday_temp = monday.temp
+# # print(convert_CtoF(monday_temp))
 
-monday = data[data.day == "Monday"]
-monday_temp = monday.temp
-# print(convert_CtoF(monday_temp))
+# # print(convert_CtoF(data.temp == monday))
 
-# print(convert_CtoF(data.temp == monday))
+# # print(monday.condition)
 
-# print(monday.condition)
+# # print(data[data.temp == max])
 
-# print(data[data.temp == max])
+# # create a dataframe from scratch
 
-# create a dataframe from scratch
+# data_dict = {
+#     "students" : ["Amy","James","Angela"],
+#     "scores" : [76,56,65]
+# }
 
-data_dict = {
-    "students" : ["Amy","James","Angela"],
-    "scores" : [76,56,65]
+# new =pd.DataFrame(data_dict)
+# # print(new)
+
+# new.to_csv("new_data.csv")
+
+#build new datafram with only the totals for fur color and then export to CSV
+
+raw_data = pd.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+
+colors = raw_data["Primary Fur Color"]
+gray = sum(colors == "Gray")
+red = sum(colors == "Cinnamon")
+black = sum(colors == "Black")
+
+q_dict = {
+    "Fur Color" : ["gray","red","black"],
+    "Count" : [gray,red,black]
 }
-
-new =pd.DataFrame(data_dict)
-# print(new)
-
-new.to_csv("new_data.csv")
-
+result = pd.DataFrame(q_dict)
+print(result)
