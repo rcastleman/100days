9496406b8a0c769234d5bdc4765e9a0724d169cf
Randy Castleman <rcastleman@gmail.com>
2022-08-18 22:45:18 -0400

    starting build...

--- us-states-game/main.py
+++ us-states-game/main.py
@@ -6,7 +6,15 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+# get all the x,y coordinates of the states
+# def get_mouse_click_coor(x,y):
+#     print(x,y)
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
 
+answer_state = screen.textinput(title = "Guess the State",prompt = "What's another state's name?")
 
-
-screen.exitonclick()
+#TODO check if answer_state.Title() is in list
+#IF YES
+#TODO write state on to screen at x,y value
+#TODO create state counter 
