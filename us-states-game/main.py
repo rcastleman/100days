56f8390a047368242f6d87a70cb8beb90d65b2e6
Randy Castleman <rcastleman@gmail.com>
2022-08-19 20:29:41 -0400

    implementing user input

--- us-states-game/main.py
+++ us-states-game/main.py
@@ -9,12 +9,30 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-answer_state = screen.textinput(title = "Guess the State",
-prompt = "What's another state's name?")
+answer_state = screen.textinput(title = "Guess the State",prompt = "What's another state's name?")
 
 user_input = answer_state.title()
 
 states = pd.read_csv("50_states.csv")
+
+# print(states.head())
+
+class state_object(turtle.Turtle):
+    
+    def __init__(self,user_input):
+        super().__init__()
+        self.shape("square")
+        self.turtle.shapesize(1,2)
+        self.write(turtle.write("TEST TEXT", True, align="center"))
+        self.penup()
+        state_location = (states["x"],states["y"])
+        self.goto(state_location)
+        
+#TODO check if answer_state.Title() is in list of states
+#IF YES
+    #TODO write Guess to screen at x,y value
+#IF NO
+    #ask user to make another guess
 
 def check_input(user_input):
     for state in states["state"]:
@@ -23,7 +41,7 @@ def check_input(user_input):
         else:
             print("N")
 
-print(check_input(user_input))
+# print(check_input(user_input))
 
 # print(states["state"])
 
@@ -32,11 +50,6 @@ print(check_input(user_input))
 # else:
 #     print("Wrong")
 
-#TODO check if answer_state.Title() is in list of states
-#IF YES
-    #TODO write Guess to screen at x,y value
-#IF NO
-    #ask user to make another guess
 #TODO record correct guesses in a list
 #TODO keep track of score
 
