aba99c5fa47885a857d9885cad7dd14c45256357
Randy Castleman <rcastleman@gmail.com>
2022-08-21 14:00:40 -0400

    kwargs exercises

---
+++ day_27 (tkinter)/playground.py
@@ -0,0 +1,25 @@
+def add(*args):
+    total = 0
+    for num in args:
+        total += num
+    return total
+
+# print(add(1,2,3,4,5,6,7,8,9,10))
+
+def calculate(n,**kwargs):
+    n += kwargs['add']
+    n *= kwargs['multiply']
+    return n
+
+# print(calculate(2,add=3,multiply=5))
+
+class Car:
+
+    def __init__(self,**kw) -> None:
+        self.make = kw["make"]
+        self.model = kw["model"]
+
+my_car = Car(make = "Nissan",model="GTR")
+
+print(f"\nThe model of my car is {my_car.model}\n")
+
