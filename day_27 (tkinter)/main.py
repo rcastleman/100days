5747f02a345994c3e418e0c9669cc414d28eb901
Randy Castleman <rcastleman@gmail.com>
2022-08-21 15:10:55 -0400

    buttons and lables and entries

--- day_27 (tkinter)/main.py
+++ day_27 (tkinter)/main.py
@@ -1,10 +1,26 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI Window")
 window.minsize(width=500,height=300)
 #label
-my_label = tkinter.Label(text = "I am a Label",font = ("Arial",24,"bold"))
-my_label.pack(side = "bottom")
+my_label = Label(text = "I am a Label",
+font = ("Arial",24,"bold"))
+my_label.pack()
+#update label text
+my_label["text"] = "New Text"
+
+def button_clicked():
+    new_text = input.get()
+    my_label["text"] = new_text
+
+#button
+button = Button(text = "Click Me",
+font = ("Arial",24,"bold"),
+command=button_clicked)
+button.pack()
+
+input = Entry(relief="sunken")
+input.pack()
 
 window.mainloop()
