c041311c8128fc082308450155a95ee7bb2cf81b
Randy Castleman <rcastleman@gmail.com>
2022-08-21 18:20:33 -0400

    exercise outline

--- day_27 (tkinter)/tkinter_exercise.py
+++ day_27 (tkinter)/tkinter_exercise.py
@@ -4,7 +4,20 @@ window = Tk()
 window.title("Miles to Km Converter")
 window.minsize(width=500, height=500)
 
+#TODObutton click function
 
+#TODO conversion function
 
+#TODO entry: miles (c=1,r=0)
+
+#TODO label:miles (c=2,r=0)
+
+#TODO label: equals (c=0,r=1)
+
+#TODO label: output (c=1,r=1)
+
+#TODO label: Km (c=2,r=1)
+
+#TODO button: Calculate (c=1,r=3)
 
 window.mainloop()
