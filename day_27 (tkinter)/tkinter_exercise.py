c16967a0e84d154ee946ae04238500a8573950e3
Randy Castleman <rcastleman@gmail.com>
2022-08-21 19:30:42 -0400

    converter completed

--- day_27 (tkinter)/tkinter_exercise.py
+++ day_27 (tkinter)/tkinter_exercise.py
@@ -1,13 +1,13 @@
 from tkinter import *
 
 # conversion function
-def conversion():
+def miles_to_km():
     #string -> float for calculation
     miles = float(miles_input.get())
     #calculation
     km = miles * 1.609
     #float -> string for ouput
-    output_label.config(text=f"{km}")
+    kilometer_result_label.config(text=f"{km}")
 
 window = Tk()
 window.title("Miles to Km Converter")
@@ -22,19 +22,19 @@ miles_label = Label(text="Miles")
 miles_label.grid(column=2,row=0)
 
 # label: equals (c=0,r=1)
-equals_label = Label(text="is equal to")
-equals_label.grid(column=0,row=1)
+is_equal_label = Label(text="is equal to")
+is_equal_label.grid(column=0,row=1)
 
 # label: output (c=1,r=1)
-output_label = Label(text="0")
-output_label.grid(column=1,row=1)
+kilometer_result_label = Label(text="0")
+kilometer_result_label.grid(column=1,row=1)
 
 # label: Km (c=2,r=1)
-km_label = Label(text="Km")
-km_label.grid(column=2,row=1)
+kilometer_label = Label(text="Km")
+kilometer_label.grid(column=2,row=1)
 
 # button: Calculate (c=1,r=3)
-button = Button(text="Calculate",command = conversion)
-button.grid(column=1,row=2)
+calculate_button = Button(text="Calculate",command = miles_to_km)
+calculate_button.grid(column=1,row=2)
 
 window.mainloop()
