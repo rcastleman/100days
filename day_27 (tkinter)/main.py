585fccaa009b372b9575175372f2f2fe202d5168
Randy Castleman <rcastleman@gmail.com>
2022-08-20 22:48:43 -0400

    tkinter 101

--- day_27 (tkinter)/main.py
+++ day_27 (tkinter)/main.py
@@ -2,6 +2,9 @@ import tkinter
 
 window = tkinter.Tk()
 window.title("My First GUI Window")
-
+window.minsize(width=500,height=300)
+#label
+my_label = tkinter.Label(text = "I am a Label",font = ("Arial",24,"bold"))
+my_label.pack(side = "bottom")
 
 window.mainloop()
