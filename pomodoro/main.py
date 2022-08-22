6622fd199b8f2b07e3e5ea5a334787dc191d40d6
Randy Castleman <rcastleman@gmail.com>
2022-08-21 21:52:48 -0400

    pomodoro solution implementation with countdown

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -1,9 +1,8 @@
 from itertools import count
 from tkinter import *
 import time
-import datetime
 
-# ---------------------------- CONSTANTS ------------------------------- #
+# CONSTANTS #
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
@@ -13,8 +12,6 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 # def countdown():
 #     t = 25*60
@@ -27,17 +24,11 @@ LONG_BREAK_MIN = 20
 #         time.sleep(1)
 #         t -= 1
 
-# ---------------------------- START BUTTON --------------------------------- # 
-
-
-
-
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
-
-# countdown_label = Label(text = "✓",fg = GREEN)
-# countdown_label.grid(column=1,row=3)
-
-# ---------------------------- UI SETUP ------------------------------- #
+def count_down(count):
+    # print(count)
+    canvas.itemconfig(timer_text,text=count)
+    if count > 0:
+        window.after(1000,count_down,count-1)
 
 # window
 window = Tk()
@@ -50,15 +41,18 @@ title_label.grid(column=1,row=0)
 canvas = Canvas(width=200, height=224,bg = YELLOW,highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100,112,image=tomato_img)
-canvas.create_text(100,130,text="00:00",
-fill = "white",
-font = (FONT_NAME,35,"bold"))
+timer_text = canvas.create_text(100,130,text="00:00",fill = "white",font = (FONT_NAME,35,"bold"))
 canvas.grid(column=1,row=1)
 
-start_button = Button(text="Start")
+count_down(5)
+
+start_button = Button(text="Start",highlightthickness=0)
 start_button.grid(column=0,row=2)
 
-reset_button = Button(text="Reset")
+reset_button = Button(text="Reset",highlightthickness=0)
 reset_button.grid(column=2,row=2)
+
+check_marks = Label(text = "✓",fg = GREEN,bg = YELLOW)
+check_marks.grid(column=1,row=3)
 
 window.mainloop()
