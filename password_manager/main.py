5cd8a223412fdbf28539e0b77fb4d30e10ff98d6
Randy Castleman <rcastleman@gmail.com>
2022-08-22 21:28:16 -0400

    initial i/o

--- password_manager/main.py
+++ password_manager/main.py
@@ -1,9 +1,32 @@
+from asyncio.proactor_events import _ProactorBaseWritePipeTransport
+from readline import insert_text
 from tkinter import *
+from tkinter.ttk import Separator
+from turtle import clear
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# using ADD button, save entered info (website,email,password) into data.txt
+# separator = "|" or whatever
+
+# user tkinter insert() method
+# use Write to file method
+# use delete() to clear entries after Add button is used
+
+def save():
+    with open ("data.txt","a") as file:
+        site = website_entry.get()
+        email = user_entry.get()
+        password = password_entry.get()
+        file.write(f"{site} | {email} | {password}")
+
+def clear_fields():
+    website_entry.delete(0,END)
+    user_entry.delete(0,END)
+    password_entry.delete(0,END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -23,6 +46,7 @@ website_label.grid(column=0,row=1)
 #website entry (col = 1,row = 1,columnspan = 2,width = 35 )
 website_entry = Entry(width=35)
 website_entry.grid(column=1,row=1,columnspan=2)
+website_entry.focus()
 
 #user label (0,2)
 user_label = Label(text="Email/Username: ")
@@ -31,6 +55,7 @@ user_label.grid(column=0,row=2)
 #user entry (col = 1,row = 2,columnspan = 2)
 user_entry = Entry(width=35)
 user_entry.grid(column=1,row=2,columnspan=2)
+user_entry.insert(0,"rcastleman@gmail.com")
 
 #password label (0,3)
 password_label = Label(text="Password: ")
@@ -45,7 +70,7 @@ generate_button = Button(text = "Generate Password")
 generate_button.grid(column=2,row=3)
 
 #add button (1,4,colspan = 2,width = 36)
-add_button = Button(text = "Add to Clipboard",width = 36)
+add_button = Button(text = "Add to Clipboard",width = 36,command=save)
 add_button.grid(column=1,row=4,columnspan=2)
 
 window.mainloop()
