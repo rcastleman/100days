81f3ed3fe6b98a6096c61859fb83047b9319b1fc
Randy Castleman <rcastleman@gmail.com>
2022-08-22 20:01:56 -0400

    beginning password manager

--- password_manager/main.py
+++ password_manager/main.py
@@ -1,5 +1,18 @@
+from tkinter import *
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+
+canvas = Canvas(height=200,width=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100,100, image=logo_img)
+canvas.pack()
+
+window.mainloop()
