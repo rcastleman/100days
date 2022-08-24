bec037e191651dea8790b3b4a614129a1cbf20b1
Randy Castleman <rcastleman@gmail.com>
2022-08-23 21:51:46 -0400

    error catching types

--- day_30 (error catching)/main.py
+++ day_30 (error catching)/main.py
@@ -1,8 +1,28 @@
 # with open("a_file.txt") as file:
 #     file.read()
 
-try:
-    file = open("a_file.txt")
-except:
-    # print("There was an error")
-    file = open("a_file.txt","w")
+# try:
+#     file = open("a_file.txt")
+#     a_dictionary = {"key":"value"}
+#     print(a_dictionary["key"])
+# except KeyError as error_message:
+#     print(f"The key {error_message} does not exist")
+# except:
+#     file = open("a_file.txt","w")
+#     file.write("something")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     file.close()
+#     print("File was closed")
+#     raise KeyError
+
+height = float(input("height: "))
+weight = float(input("weight: "))
+
+if height > 3:
+    raise ValueError ("Human height < 3m")
+
+bmi = weight / height **2
+print(bmi)
