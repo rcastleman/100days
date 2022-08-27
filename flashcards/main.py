b53c95e72f665d660c6e72dd1fb99d7c6b8dd185
Randy Castleman <rcastleman@gmail.com>
2022-08-26 23:03:34 -0400

    UI setup

--- flashcards/main.py
+++ flashcards/main.py
@@ -21,8 +21,12 @@ window.config(padx = 50,pady = 50,bg = BACKGROUND_COLOR)
 canvas = Canvas(width=800,height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400,263,image=card_front_img)
+canvas.create_text(400,150,text="Title",font=("Arial",40,"italic"))
+canvas.create_text(400,263,text="Word",font=("Arial",60,"bold"))
+
 canvas.config(bg=BACKGROUND_COLOR,highlightthickness=0)
 canvas.grid(row=0,column=0)
+
 
 # flash_back = Canvas(height=526,width=800)
 # logo_img = PhotoImage(file="logo.png")
