97f8fce8a8d51115808314e046879dc1d39b28f4
Randy Castleman <rcastleman@gmail.com>
2022-08-26 23:09:02 -0400

    buttons added

--- flashcards/main.py
+++ flashcards/main.py
@@ -23,10 +23,17 @@ card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400,263,image=card_front_img)
 canvas.create_text(400,150,text="Title",font=("Arial",40,"italic"))
 canvas.create_text(400,263,text="Word",font=("Arial",60,"bold"))
-
 canvas.config(bg=BACKGROUND_COLOR,highlightthickness=0)
-canvas.grid(row=0,column=0)
+canvas.grid(row=0,column=0,columnspan=2)
 
+#X Mark
+cross_image = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=cross_image)
+unknown_button.grid(row=1,column=0)
+
+check_image = PhotoImage(file="images/right.png")
+known_button = Button(image=check_image)
+known_button.grid(row=1,column=1)
 
 # flash_back = Canvas(height=526,width=800)
 # logo_img = PhotoImage(file="logo.png")
