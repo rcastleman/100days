2e9de9b49693766c7cc2336dfa11a5ac302c7de0
Randy Castleman <rcastleman@gmail.com>
2022-08-26 23:00:17 -0400

    setup UI

--- flashcards/main.py
+++ flashcards/main.py
@@ -12,21 +12,23 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 #------------------ UI -----------------------#
 
-card_back = PhotoImage(file="images/card_back.png")
-card_front = PhotoImage(file="images/card_front.png")
+# card_back = PhotoImage(file="/images/card_back.png")
 
 window = Tk()
 window.title("Flashcards")
-window.config(padx = 50,pady = 50)
+window.config(padx = 50,pady = 50,bg = BACKGROUND_COLOR)
 
-flash_front = Canvas(height=526,width=800)
-logo_img = PhotoImage(file="logo.png")
-flash_front.create_image(300,400, image=card_front)
-flash_front.grid(column=1,row=0)
+canvas = Canvas(width=800,height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+canvas.create_image(400,263,image=card_front_img)
+canvas.config(bg=BACKGROUND_COLOR,highlightthickness=0)
+canvas.grid(row=0,column=0)
 
-flash_back = Canvas(height=526,width=800)
-logo_img = PhotoImage(file="logo.png")
-flash_back.create_image(300,400, image=card_front)
-flash_back.grid(column=1,row=0)
+# flash_back = Canvas(height=526,width=800)
+# logo_img = PhotoImage(file="logo.png")
+# flash_back.create_image(300,400, image=card_front)
+# flash_back.grid(column=1,row=0,columnspan=2)
 
 # button = Button(image=my_image, highlightthickness=0)
+
+window.mainloop()
