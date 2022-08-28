06153e3209054b7fdb3af388d7bbcad73938854f
Randy Castleman <rcastleman@gmail.com>
2022-08-27 22:22:48 -0400

    flashcards

--- flashcards/main.py
+++ flashcards/main.py
@@ -14,7 +14,10 @@ to_learn = data.to_dict(orient="records")
 
 def next_card():
     current_card = random.choice(list(to_learn))
-    print(current_card['French'])
+    canvas.itemconfig(card_title,text="French")
+    canvas.itemconfig(card_word,text=current_card['French'])
+    #time
+    #change to English
 
 #------------------ UI -----------------------#
 
@@ -27,8 +30,8 @@ window.config(padx = 50,pady = 50,bg = BACKGROUND_COLOR)
 canvas = Canvas(width=800,height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400,263,image=card_front_img)
-canvas.create_text(400,150,text="Title",font=("Arial",40,"italic"))
-canvas.create_text(400,263,text=next_card(),font=("Arial",60,"bold"))
+card_title = canvas.create_text(400,150,text="Title",font=("Arial",40,"italic"))
+card_word = canvas.create_text(400,263,text="word",font=("Arial",60,"bold"))
 canvas.config(bg=BACKGROUND_COLOR,highlightthickness=0)
 canvas.grid(row=0,column=0,columnspan=2)
 
@@ -48,5 +51,7 @@ known_button.grid(row=1,column=1)
 # flash_back.grid(column=1,row=0,columnspan=2)
 
 # button = Button(image=my_image, highlightthickness=0)
+
+next_card()
 
 window.mainloop()
