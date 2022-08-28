45beb01b49af0f07291cd95b3eb4e4099e765c01
Randy Castleman <rcastleman@gmail.com>
2022-08-28 15:56:31 -0400

    output list modification

--- flashcards/main.py
+++ flashcards/main.py
@@ -4,14 +4,37 @@ from tkinter import *
 from turtle import clear
 import random
 import pandas as pd
+import os.path
 
 
 BACKGROUND_COLOR = "#B1DDC6"
 
 #------------------ read data -----------------------#
 
-data = pd.read_csv("data/french_words.csv")
+if os.path.exists("data/words_to_learn.csv"):
+    data = pd.read_csv("data/words_to_learn.csv")
+else:
+    data = pd.read_csv("data/french_words.csv")
+
+learned_words = []
+
+#dataframe -> list
+output_list = data.values.tolist()
+print(output_list)
+# remove learned word from list
+
+def mark_for_removal(word):
+    """adds a word to the learned_words list, marking it for removal"""
+    learned_words.append(word)
+
+def remove_word():
+    """removes word from the output_list"""
+    for word in output_list:
+        if word in learned_words:
+            output_list.remove(word)
+
 to_learn = data.to_dict(orient="records")
+
 current_card = {}
 
 def next_card():
@@ -34,7 +57,6 @@ window.title("Flashcards")
 window.config(padx = 50,pady = 50,bg = BACKGROUND_COLOR)
 
 flip_timer = window.after(3000,func=flip_card)
-
 
 canvas = Canvas(width=800,height=526)
 card_front_img = PhotoImage(file = "images/card_front.png")
