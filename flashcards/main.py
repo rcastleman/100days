4a13a2f4b618011a7b59f78d18e5ff7bcf37ef13
Randy Castleman <rcastleman@gmail.com>
2022-08-28 15:07:42 -0400

    background color changes

--- flashcards/main.py
+++ flashcards/main.py
@@ -11,15 +11,18 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 data = pd.read_csv("data/french_words.csv")
 to_learn = data.to_dict(orient="records")
+current_card = {}
 
 def next_card():
+    global current_card
     current_card = random.choice(list(to_learn))
-    canvas.itemconfig(card_title,text="French")
-    canvas.itemconfig(card_word,text=current_card["French"])
+    canvas.itemconfig(card_title,text="French",fill="black")
+    canvas.itemconfig(card_word,text=current_card["French"],fill="black")
 
 def flip_card():
-    canvas.itemconfig(card_title, text="English")
-    canvas.itemconfig(card_word,text=current_card["English"])
+    canvas.itemconfig(card_title, text="English",fill = "white")
+    canvas.itemconfig(card_word,text=current_card["English"],fill="white")
+    canvas.itemconfig(card_background,image = card_back_img)
 
 #------------------ UI -----------------------#
 window = Tk()
@@ -28,8 +31,9 @@ window.config(padx = 50,pady = 50,bg = BACKGROUND_COLOR)
 window.after(3000,func=flip_card)
 
 canvas = Canvas(width=800,height=526)
-card_front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400,263,image=card_front_img)
+card_front_img = PhotoImage(file = "images/card_front.png")
+card_back_img = PhotoImage(file = "images/card_back.png")
+card_background = canvas.create_image(400,263,image=card_front_img)
 card_title = canvas.create_text(400,150,text="Title",font=("Arial",40,"italic"))
 card_word = canvas.create_text(400,263,text="word",font=("Arial",60,"bold"))
 canvas.config(bg=BACKGROUND_COLOR,highlightthickness=0)
