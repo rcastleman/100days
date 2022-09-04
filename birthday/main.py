e91c1ab5e4528d1cce94f3c8fe353409fec37357
Randy Castleman <rcastleman@gmail.com>
2022-09-04 16:37:59 -0400

    done

--- birthday/main.py
+++ birthday/main.py
@@ -8,20 +8,6 @@ from email_via_python import send_email
 import csv
 import pandas as pd
 
-#HINT 4: Then you could compare and see if today's month/day tuple matches one of the keys in birthday_dict like this:
-# if (today_month, today_day) in birthdays_dict:
-
-# TODO If there is a match, pick a random letter (letter_1.txt/letter_2.txt/letter_3.txt) from letter_templates and replace the [NAME] with the person's actual name from birthdays.csv
-# HINT 1: Think about the relative file path to open each letter. 
-# HINT 2: Use the random module to get a number between 1-3 to pick a randome letter.
-# HINT 3: Use the replace() method to replace [NAME] with the actual name. https://www.w3schools.com/python/ref_string_replace.asp
-
-# TODO Send the letter generated in step 3 to that person's email address.
-# HINT 1: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
-# HINT 2: Remember to call .starttls()
-# HINT 3: Remember to login to your email service with email/password. Make sure your security setting is set to allow less secure apps.
-# HINT 4: The message should have the Subject: Happy Birthday then after \n\n The Message Body.
-
 now = dt.datetime.now()
 day = now.day
 month = now.month
