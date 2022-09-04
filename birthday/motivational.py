f2f555fba3da9d8e434c41afbf6b1382cb2be359
Randy Castleman <rcastleman@gmail.com>
2022-09-03 22:15:09 -0400

    motivational exercise...

--- birthday/motivational.py
+++ birthday/motivational.py
@@ -10,3 +10,15 @@ import pandas as pd
 # TODO open quotes.txt and get LIST of quotes
 # TODO send email with random quote 
 
+now = dt.datetime.now()
+day = now.weekday()
+
+day_dict = {'Monday': 0, 'Tuesday': 1, 'Wednesday': 2, 'Thursday': 3, 'Friday': 4, 'Saturday': 5, 'Sunday': 6}
+
+target_day = "Saturday"
+
+def send_email():
+    print("sending an email!")
+
+if day == day_dict[target_day]:
+    send_email()
