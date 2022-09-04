92bd9038496fc9d7d301fb143fcb931012b0ffe6
Randy Castleman <rcastleman@gmail.com>
2022-09-03 22:06:20 -0400

    exercise TODOs

--- birthday/motivational.py
+++ birthday/motivational.py
@@ -1,6 +1,12 @@
+from calendar import week
 from multiprocessing import connection
 import smtplib
 import datetime as dt
 import random
 import pandas as pd
+
+# TODO obtain current day of week
+# TODO check to see if day of week = Target Day
+# TODO open quotes.txt and get LIST of quotes
+# TODO send email with random quote 
 
