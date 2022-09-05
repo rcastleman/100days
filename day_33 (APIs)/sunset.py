fa5a66c3cb9327c8853459d71813afeb76da0aa2
Randy Castleman <rcastleman@gmail.com>
2022-09-04 22:08:45 -0400

    ISS Tracker complete

--- day_33 (APIs)/sunset.py
+++ day_33 (APIs)/sunset.py
@@ -1,9 +1,17 @@
 from ast import FormattedValue
+from pickle import TRUE
 import requests
 from datetime import datetime
 from email_engine import send_email
+import time
 
-#----------- ISS API --------------- #
+MY_LAT = 40.753270
+MY_LNG = -73.971660
+
+# MY_LAT = 10.0
+# MY_LNG = -59.0
+
+#------------------- ISS API -------------------- #
 response = requests.get(url = "http://api.open-notify.org/iss-now.json")
 data  = response.json()
 
@@ -13,39 +21,42 @@ iss_longitude = float(data['iss_position']['longitude'])
 
 print(f"ISS lat: {iss_latitude}")
 print(f"ISS lng: {iss_longitude}")
+print(f"My Lat: {MY_LAT}")
+print(f"My Lng: {MY_LNG}")
 
-#----------- Sunrise / Sunset  API --------------- #
+#------------- Sunrise / Sunset  API -------------- #
 
-# MY_LAT = 40.753270
-# MY_LNG = -73.971660
+def is_night(): 
+    parameters = {"lat":MY_LAT,
+    "lng": MY_LNG,
+    "formatted":0}
 
-MY_LAT = 10.0
-MY_LNG = -59.0
+    response = requests.get(url = "https://api.sunrise-sunset.org/json",params=parameters)
+    response.raise_for_status()
+    data  = response.json()
 
-parameters = {"lat":MY_LAT,
-"lng": MY_LNG,
-"formatted":0}
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    time_now = datetime.now().hour
 
-response = requests.get(url = "https://api.sunrise-sunset.org/json",params=parameters)
-response.raise_for_status()
-data  = response.json()
+    print(f"sunrise: {sunrise}")
+    print(f"sunset: {sunset}")
+    print(f"time now: {time_now}")
 
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    if time_now >= sunset or time_now <= sunrise:
+        return True
 
-print(f"sunrise: {sunrise}")
-print(f"sunset: {sunset}")
-time_now = datetime.now()
-print(f"local time: {time_now.hour}")
-
-#if the ISS within 5 degrees of current lat or long
-#send email saying "look up"
-
-subject = "ISS overhead!"
-body = "The ISS is passing overhead.  Go outside and take a look!"
+#-------------- ISS PROXIMITY TEST --------------------#
 
 def is_overhead():
-    if abs(iss_latitude - MY_LAT) < 5 or abs(iss_longitude - MY_LNG) < 5:
-        send_email(subject=subject,body=body)
+    if abs(iss_latitude - MY_LAT) < 5 and abs(iss_longitude - MY_LNG) < 5:
+        return True
 
-is_overhead()
+#--------------------- SEND EMAIL --------------------#
+
+while True:
+    time.sleep(60)
+    if is_night and is_overhead:
+        subject = "ISS overhead!"
+        body = "The ISS is passing overhead.  Go outside and take a look!"
+        send_email(subject=subject,body=body)
