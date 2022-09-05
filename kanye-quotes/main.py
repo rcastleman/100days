1457a0b2cb5ae321a1c68b4bed8712d865af658a
Randy Castleman <rcastleman@gmail.com>
2022-09-04 20:25:43 -0400

    kanye done

--- kanye-quotes/main.py
+++ kanye-quotes/main.py
@@ -1,9 +1,6 @@
 from tkinter import *
 import requests
 
-
-
-
 window = Tk()
 window.title("Kanye Says...")
 window.config(padx=50, pady=50)
@@ -26,6 +23,5 @@ kanye_img = PhotoImage(file="kanye.png")
 kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
 kanye_button.grid(row=1, column=0)
 
-    
 get_quote()
 window.mainloop()
