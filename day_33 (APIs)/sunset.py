c5277d18daf246be7c672cbdbe50755055a85f32
Randy Castleman <rcastleman@gmail.com>
2022-09-04 21:01:09 -0400

    times

--- day_33 (APIs)/sunset.py
+++ day_33 (APIs)/sunset.py
@@ -1,12 +1,21 @@
+from ast import FormattedValue
 import requests
+from datetime import datetime
 
 MY_LAT = 40.753270
 MY_LNG = -73.971660
 
 parameters = {"lat":MY_LAT,
-"lng": MY_LNG}
+"lng": MY_LNG,"formatted":0}
 
 response = requests.get(url = "https://api.sunrise-sunset.org/json",params=parameters)
 response.raise_for_status()
-# data  = response.json()
-# print(data)
+data  = response.json()
+sunrise = data["results"]["sunrise"]
+sunset = data["results"]["sunset"]
+
+#split out the TIME then the HOUR from the time
+print(sunrise.split("T")[1].split(":")[0])
+
+# time_now = datetime.now()
+# print(time_now)
