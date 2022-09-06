2efad4c057f64e620d79e9ec0699415fa58a3c00
Randy Castleman <rcastleman@gmail.com>
2022-09-05 22:58:32 -0400

    UI complete

--- day_34 (quizzler)/ui.py
+++ day_34 (quizzler)/ui.py
@@ -11,21 +11,25 @@ def mark_true():
 class QuizInterface:
 
     def __init__(self) -> None:
+
         self.window = Tk()
         self.window.title("Quizzler")
 
         self.window.config(padx=20,pady=20,bg=THEME_COLOR)
 
-        self.score_label = Label(text = "Score",fg = "white",bg = THEME_COLOR,font = ("courier",20))
+        self.score_label = Label(text = "Score: 0",fg = "white",bg = THEME_COLOR,font = ("courier",20))
         self.score_label.grid(column=1,row=0)
 
-        self.canvas = Canvas(width=300,height=250)
-        self.question_text = self.canvas.create_text(100,130,text="TEST TEXT",fill = "black",font=("courier",20))
-        self.canvas.grid(column=0,row=1,columnspan=2)
+        self.canvas = Canvas(width=300,height=250,bg="white")
+        self.question_text = self.canvas.create_text(150,125,text="Some Question Text...",fill = THEME_COLOR,font=("arial",20,"italic"))
+        self.canvas.grid(column=0,row=1,columnspan=2,pady=50)
 
-        self.false_button = Button(text="False",command=mark_false)
+        false_image = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_image,highlightthickness=0,command=mark_false)
         self.false_button.grid(column=1,row=2)
-        self.true_button = Button(text="True",command=mark_true)
+
+        true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_image,highlightthickness=0,command=mark_true)
         self.true_button.grid(column=0,row=2)
 
         self.window.mainloop()
