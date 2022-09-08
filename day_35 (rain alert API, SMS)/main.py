460b5b350f0b1e90ade2587c4e52e783905765a8
Randy Castleman <rcastleman@gmail.com>
2022-09-07 22:11:23 -0400

    grabbing particular elements from teh JSON file

--- day_35 (rain alert API, SMS)/main.py
+++ day_35 (rain alert API, SMS)/main.py
@@ -7,7 +7,7 @@ one_call_API = "52b9cd8ce0541eaea09c5c01dbbf4609"
 four_days_API = "b299b8818c68292f1fa725c78a75a35e"
 NYC_LAT = 40.753270
 NYC_LNG = -73.971660
-COUNT = 8
+COUNT = 3
 
 five_day = f"https://api.openweathermap.org/data/2.5/forecast?lat={NYC_LAT}&lon={NYC_LNG}&appid={API_KEY}&units=imperial&cnt={COUNT}"
 
@@ -20,6 +20,9 @@ five_day = f"https://api.openweathermap.org/data/2.5/forecast?lat={NYC_LAT}&lon=
 
 response = requests.get(url = five_day)
 data  = response.json()
-print(data)
+for element in range(3):
+    id = data["list"][element]["weather"][0]["id"]
+    temp = data["list"][element]["main"]["temp"]
+    print(f"The weather condition ID is {id} and the temp is {temp}")
 
 # print(five_day)
