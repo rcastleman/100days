2ef26157f9bfa14464848c32b2371f80eefe629d
Randy Castleman <rcastleman@gmail.com>
2022-09-07 20:27:32 -0400

    weather app commence

--- day_35 (rain alert API, SMS)/main.py
+++ day_35 (rain alert API, SMS)/main.py
@@ -3,3 +3,10 @@
 
 API_KEY = "8f21e2683deb718640c8241bb5e63746"
 
+NYC_LAT = 40.753270
+NYC_LNG = -73.971660
+
+API_URL = f"https://api.openweathermap.org/data/2.5/weather?lat={NYC_LAT}&lon={NYC_LNG}&appid={API_KEY}&units=imperial"
+
+print(API_URL)
+
