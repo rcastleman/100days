f99f73371722fa50bbe600f192bbc74e062c0a6b
Randy Castleman <rcastleman@gmail.com>
2022-09-07 22:29:16 -0400

    checking condition codes for rain

--- day_35 (rain alert API, SMS)/main.py
+++ day_35 (rain alert API, SMS)/main.py
@@ -1,3 +1,5 @@
+from operator import truediv
+from xmlrpc.client import FastMarshaller
 import requests
 
 # https://home.openweathermap.org/api_keys
@@ -7,7 +9,7 @@ one_call_API = "52b9cd8ce0541eaea09c5c01dbbf4609"
 four_days_API = "b299b8818c68292f1fa725c78a75a35e"
 NYC_LAT = 40.753270
 NYC_LNG = -73.971660
-COUNT = 3
+COUNT = 8
 
 five_day = f"https://api.openweathermap.org/data/2.5/forecast?lat={NYC_LAT}&lon={NYC_LNG}&appid={API_KEY}&units=imperial&cnt={COUNT}"
 
@@ -20,9 +22,26 @@ five_day = f"https://api.openweathermap.org/data/2.5/forecast?lat={NYC_LAT}&lon=
 
 response = requests.get(url = five_day)
 data  = response.json()
-for element in range(3):
-    id = data["list"][element]["weather"][0]["id"]
-    temp = data["list"][element]["main"]["temp"]
-    print(f"The weather condition ID is {id} and the temp is {temp}")
 
-# print(five_day)
+condition_list = []
+for element in range(COUNT):
+    id = data["list"][element]["weather"][0]["id"]
+    condition_list.append(id)
+
+print(condition_list)
+
+will_rain = False
+
+for e in condition_list:
+    if e <=700:
+        will_rain = True
+
+if will_rain:
+    print("Bring an umbrella")
+else:
+    print("No need for an umbrella")
+
+# for element in range(3):
+#     id = data["list"][element]["weather"][0]["id"]
+#     temp = data["list"][element]["main"]["temp"]
+#     print(f"The weather condition ID is {id} and the temp is {temp}")
