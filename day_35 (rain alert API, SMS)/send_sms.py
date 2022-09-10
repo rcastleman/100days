0fec54954501f03183998bdb576364316cac9686
Randy Castleman <rcastleman@gmail.com>
2022-09-09 22:46:51 -0400

    wrestling with Twilio ... still

--- day_35 (rain alert API, SMS)/send_sms.py
+++ day_35 (rain alert API, SMS)/send_sms.py
@@ -1,13 +1,15 @@
 import os
+from secrets import SID
+from telnetlib import AUTHENTICATION
 from twilio.rest import Client
+import secrets
 
-# TWILIO_ACCOUNT_SID = ""
-# TWILIO_AUTH_TOKEN = ""
 TWILIO_PHONE = "+19855895764"
 TARGET_PHONE = "+14348254811"
 
-account_sid = ""
-auth_token = ""
+account_sid = secrets.SID
+auth_token = secrets.AUTH
+
 client = Client(account_sid, auth_token)
 
 message = client.messages \
