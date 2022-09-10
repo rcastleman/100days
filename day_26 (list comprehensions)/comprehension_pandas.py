c339956ea234618ee3951054f45d868c5e1a94f4
Randy Castleman <rcastleman@gmail.com>
2022-09-09 20:01:40 -0400

    'synching old files'

--- day_26 (list comprehensions)/comprehension_pandas.py
+++ day_26 (list comprehensions)/comprehension_pandas.py
@@ -7,4 +7,10 @@ student_dict = {
 
 student_df = pandas.DataFrame(student_dict)
 
-print(student_df)
+# print(student_df)
+
+# for (key,value) in student_df.items():
+#     print(value)
+
+for (index,row) in student_df.iterrows():
+    print(row.student,row.score)
