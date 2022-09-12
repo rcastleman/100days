0984de68b43eaf77c90f1ea2f75985d75fdf171e
Randy Castleman <rcastleman@gmail.com>
2022-09-11 22:15:37 -0400

    project completed

--- day_36_stocks/main.py
+++ day_36_stocks/main.py
@@ -26,33 +26,11 @@ TARGET_PHONE = "+14348254811"
 
 #----------------- hints -----------------------------#
 
-    ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
-# When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+# Use https://www.alphavantage.co/documentation/#daily
 
-#TODO 1. - Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
-
-#TODO 2. - Get the day before yesterday's closing stock price
-
-#TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
-
-#TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
-
-#TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
-
-    ## STEP 2: https://newsapi.org/ 
-    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
-
-#TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
-
-#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
-
-
-    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
+#  https://newsapi.org/ 
+# STEP 3: Use twilio.com/docs/sms/quickstart/python
     #to send a separate message with each article's title and description to your phone number. 
-
-#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
-
-#TODO 9. - Send each article as a separate message via Twilio. 
 
 
 #Optional TODO: Format the message like this: 
@@ -118,10 +96,26 @@ yesterday = 125
 # today = data["Time Series (Daily)"][today_date]["4. close"]
 # yesterday = data["Time Series (Daily)"][yesterday_date]["4. close"]
 ratio = 100*(abs(float(today)/float(yesterday))-1)
+limit = 3
 if ratio > TRIGGER:
     news_response = requests.get(NEWS_ENDPOINT,params=news_parameters)
     news_response.raise_for_status()
     news_data = news_response.json()
-    articles = news_data["articles"][0:3]
-    formatted_articles = [f"Headline: {article['title']}.\nDescription: {article['description']}" for article in articles]
-    print(formatted_articles)
+    articles = news_data["articles"][0:limit]
+    # formatted_articles = [f"Headline: {article['title']}.\nDescription: {article['description']}" for article in articles]
+    formatted_articles = [f"Headline: {article['title']}." for article in articles]
+    # print(formatted_articles)
+
+    account_sid = secrets.SID
+    auth_token = secrets.AUTH
+    client = Client(account_sid, auth_token)
+
+    for article in formatted_articles:
+
+        message = client.messages \
+            .create(
+                body=article,
+                from_=TWILIO_PHONE,
+                to=TARGET_PHONE
+            )
+    print(message.status)
