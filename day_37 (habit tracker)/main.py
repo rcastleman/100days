0d5e5790aea7f10bde5e8067e5713746bcee2517
Randy Castleman <rcastleman@gmail.com>
2022-09-12 21:52:24 -0400

    starting ppixela

--- day_37 (habit tracker)/main.py
+++ day_37 (habit tracker)/main.py
@@ -3,6 +3,9 @@ import requests
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
-pixela_parameters = {}
+pixela_parameters = {"token":"8cQz3q5v9L6sZ*4DXUA9XM@i#KD9@e!",
+"username":"rcastleman",
+"areeTermsOfService":"yes",
+"notMinor":"yes"}
 
-response = requests.post()
+response = requests.post(pixela_endpoint,pixela_parameters)
