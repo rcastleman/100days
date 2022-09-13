211858495fd3755cf72f30585fbdc846a179084a
Randy Castleman <rcastleman@gmail.com>
2022-09-12 22:50:45 -0400

    pixela...

--- day_37 (habit tracker)/main.py
+++ day_37 (habit tracker)/main.py
@@ -2,9 +2,12 @@ from importlib.metadata import requires
 from urllib import request
 import requests
 import secrets
+from datetime import datetime
+
 
 USERNAME = "rcastleman"
 TOKEN = secrets.PIX_KEY
+GRAPH_ID = "graph1"
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
@@ -19,10 +22,32 @@ pixela_parameters = {"token":TOKEN,
 pixela_graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 graph_config = {
-    "id":"graph1",
+    "id":GRAPH_ID,
     "name":"Coding Graph",
     "unit":"days",
+    "type":"int",
     "color":"sora",
     }
 
-response = requests.push(url=pixela_graph_endpoint)
+headers = {
+    "X-USER-TOKEN":TOKEN
+}
+
+# response = requests.post(url=pixela_graph_endpoint,json=graph_config,headers=headers)
+# print(response.text)
+
+pixela_post_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime.now()
+
+print(today.strftime("%Y%m%d"))
+
+post_config = {
+    "date":today.strftime("%Y%m%d"),
+    "quantity":"1"}
+
+# response = requests.post(
+#     url=pixela_post_endpoint,
+#     json=post_config,
+#     headers=headers)
+# print(response.text)
