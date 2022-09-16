761055ff7c1c31dc1fc896898f76982936b36ecc
Randy Castleman <rcastleman@gmail.com>
2022-09-15 23:14:07 -0400

    write to sheet not working yet

--- day_38 (workout)/main.py
+++ day_38 (workout)/main.py
@@ -1,7 +1,14 @@
+# from email.message import _PayloadType
+from site import execusercustomize
 import requests
 import secrets
+from datetime import date, datetime
 
 nutritionix_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+sheety_endpoint = "https://api.sheety.co/phill/myWebsite/emails"
+
+
 
 GENDER = "male"
 WEIGHT_KG = 66.7
@@ -28,3 +35,27 @@ response = requests.post(
     headers=headers)
 result = response.json()
 print(result)
+
+
+# To create a new row in your sheet, perform a POST request to the endpoint, with your row contents as a JSON payload in the request body.
+
+# sheety_headers = {}
+
+date = date.today()
+time = datetime.now()
+time_formatted = time.strftime("%H:%M")
+exercise = result["exercises"][0]["name"]
+duration = result["exercises"][0]["duration_min"]
+calories = result["exercises"][0]["nf_calories"]
+
+# print(f"Date: {date}\ntime: {time_formatted}\nexercise: {exercise}\ncalories: {calories}")
+
+payload = {
+    date,
+    time,
+    exercise,
+    duration,
+    calories
+}
+
+response = requests.post(sheety_endpoint,json=payload)
