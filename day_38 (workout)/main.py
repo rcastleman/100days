2a846e3130c6c81f7be27f94b42f946c0409a4c8
Randy Castleman <rcastleman@gmail.com>
2022-09-15 20:47:57 -0400

    workout tracker

--- day_38 (workout)/main.py
+++ day_38 (workout)/main.py
@@ -1,1 +1,20 @@
 import requests
+import secrets
+
+
+nutritionix_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+query = input("What exercise did you do?")
+
+headers = {
+    "x_app_id":secrets.APP_ID,
+    "x_app_key":secrets.APP_KEY,
+    "x-remote-user-id":0}
+
+parameters = {
+    "x-user-jwt":x_app_id,
+    "query": query
+    }
+
+response = requests.post(url=nutritionix_endpoint,params=parameters)
+print(response.text)
