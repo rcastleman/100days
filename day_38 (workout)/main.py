9a2ea492cdf6f181d49a2850460debf324d84271
Randy Castleman <rcastleman@gmail.com>
2022-09-15 21:14:22 -0400

    nutrionix API not working yet...

--- day_38 (workout)/main.py
+++ day_38 (workout)/main.py
@@ -1,20 +1,25 @@
 import requests
 import secrets
 
-
 nutritionix_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
-query = input("What exercise did you do?")
+# query = input("What exercise did you do?")
 
 headers = {
     "x_app_id":secrets.APP_ID,
-    "x_app_key":secrets.APP_KEY,
-    "x-remote-user-id":0}
+    "x_app_key":secrets.API_KEY,
+    "x-remote-user-id":"0"}
 
 parameters = {
-    "x-user-jwt":x_app_id,
-    "query": query
-    }
+ "query":"ran 3 miles",
+ "gender":"female",
+ "weight_kg":72.5,
+ "height_cm":167.64,
+ "age":30
+}
 
-response = requests.post(url=nutritionix_endpoint,params=parameters)
+response = requests.post(
+    url=nutritionix_endpoint,
+    params=parameters,
+    headers=headers)
 print(response.text)
