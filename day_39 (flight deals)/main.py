dd94526d06ae75a1899979a01fcd473d386f45c4
Randy Castleman <rcastleman@gmail.com>
2022-09-17 20:28:44 -0400

    starting flight deals project...

--- day_39 (flight deals)/main.py
+++ day_39 (flight deals)/main.py
@@ -1,1 +1,10 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+import requests
+import os
+from dotenv import load_dotenv
+
+
+
+load_dotenv()
+
