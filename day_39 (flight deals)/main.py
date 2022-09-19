0ad47a713a22786c604afc9bc4ff46a5f32ab252
Randy Castleman <rcastleman@gmail.com>
2022-09-18 21:31:17 -0400

    more

--- day_39 (flight deals)/main.py
+++ day_39 (flight deals)/main.py
@@ -2,9 +2,6 @@
 
 # TODO Use the Flight Search and Sheety API to populate your own copy of the Google Sheet with International Air Transport Association (IATA) codes for each city. Most of the cities in the sheet include multiple airports, you want the city code (not the airport code see here).
 
-
-
-
 # TODO Use the Flight Search API to check for the cheapest flights from tomorrow to 6 months later for all the cities in the Google Sheet.
 
 # TODO If the price is lower than the lowest price listed in the Google Sheet then send an SMS to your own number with the Twilio API.
@@ -18,3 +15,8 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 from pprint import pprint
+
+
+from data_manager import DataManager
+data_manager = DataManager()
+sheet_data = data_manager.get_destination_data()
