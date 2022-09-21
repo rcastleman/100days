626fe284000c5fb31821a9f634ee607a213fc802
Randy Castleman <rcastleman@gmail.com>
2022-09-20 20:50:30 -0400

    incorporating solution

--- day_39 (flight deals)/flight_search.py
+++ day_39 (flight deals)/flight_search.py
@@ -10,21 +10,11 @@ KIWI_API_KEY = os.environ.get("KIWI_API_KEY")
 
 class FlightSearch:
 
-    def get_destination_code(self,city_name):
-        response = requests.get(KIWI_ENDPOINT)
-        code = response.json()
+    def get_destination_code(self, city_name):
+        location_endpoint = f"{KIWI_ENDPOINT}/locations/query"
+        headers = {"apikey": KIWI_API_KEY}
+        query = {"term": city_name, "location_types": "city"}
+        response = requests.get(url=location_endpoint, headers=headers, params=query)
+        results = response.json()["locations"]
+        code = results[0]["code"]
         return code
-
-# from_city = "LGA"
-# to_city = "MIA"
-# depart_date = "29/10/2022"
-# return_date = "07/11/2022"
-
-# kiwi_params = {
-#     "fly_from":from_city,
-#     "fly_to": to_city,
-#     "dateFrom": depart_date,
-#     "dateTo": return_date,
-#     }
-
-# sheet_data = [{'city': 'Berlin', 'iataCode': '', 'lowestPrice': 42, 'id': 2}, {'city': 'Cape Town', 'iataCode': '', 'lowestPrice': 378, 'id': 3}, {'city': 'Istanbul', 'iataCode': '', 'lowestPrice': 95, 'id': 4}, {'city': 'Kuala Lumpur', 'iataCode': '', 'lowestPrice': 414, 'id': 5}, {'city': 'New York', 'iataCode': '', 'lowestPrice': 240, 'id': 6}, {'city': 'Paris', 'iataCode': '', 'lowestPrice': 54, 'id': 7}, {'city': 'San Francisco', 'iataCode': '', 'lowestPrice': 260, 'id': 8}, {'city': 'Sydney', 'iataCode': '', 'lowestPrice': 551, 'id': 9}, {'city': 'Tokyo', 'iataCode': '', 'lowestPrice': 485, 'id': 10}]
