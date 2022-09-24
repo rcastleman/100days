eb73993df10d4f2b72e0e1ff5251084f0a27d873
Randy Castleman <rcastleman@gmail.com>
2022-09-23 22:01:51 -0400

    fixed KIWI endpoint problem ... now works

--- day_39 (flight deals)/flight_search.py
+++ day_39 (flight deals)/flight_search.py
@@ -1,11 +1,11 @@
-import code
 import os
 import requests
 from dotenv import load_dotenv
 load_dotenv()
 
-KIWI_ENDPOINT = "https://api.tequila.kiwi.com/v2/search"
-KIWI_TOKEN = os.environ.get("KWIW_USERID")
+# KIWI_ENDPOINT = "https://api.tequila.kiwi.com/v2/search"
+KIWI_ENDPOINT = "https://tequila-api.kiwi.com"
+# KIWI_TOKEN = os.environ.get("KWIW_USERID")
 KIWI_API_KEY = os.environ.get("KIWI_API_KEY")
 
 class FlightSearch:
@@ -15,7 +15,7 @@ class FlightSearch:
         headers = {"apikey": KIWI_API_KEY}
         query = {"term": city_name, "location_types": "city"}
         response = requests.get(url=location_endpoint, headers=headers, params=query)
-        print(response.json())
-        # results = response.json()["locations"]
-        # code = results[0]["code"]
-        # return code
+        # print(response.json())
+        results = response.json()["locations"]
+        code = results[0]["code"]
+        return code
