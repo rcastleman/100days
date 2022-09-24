30ee20bcc95d55847c781b1144bb4e779a0818cd
Randy Castleman <rcastleman@gmail.com>
2022-09-23 21:40:31 -0400

    KIWI json not working

--- day_39 (flight deals)/flight_search.py
+++ day_39 (flight deals)/flight_search.py
@@ -15,7 +15,7 @@ class FlightSearch:
         headers = {"apikey": KIWI_API_KEY}
         query = {"term": city_name, "location_types": "city"}
         response = requests.get(url=location_endpoint, headers=headers, params=query)
-        # print(response.json())
-        results = response.json()["locations"]
-        code = results[0]["code"]
-        return code
+        print(response.json())
+        # results = response.json()["locations"]
+        # code = results[0]["code"]
+        # return code
--- day_39 (flight deals)/main.py
+++ day_39 (flight deals)/main.py
@@ -12,3 +12,4 @@ if sheet_data[0]["iataCode"] == "":
 
     data_manager.destination_data = sheet_data
     data_manager.update_destination_codes()
+    
