6486a686e79dbecb57a8e581a6b20a3d99884150
Randy Castleman <rcastleman@gmail.com>
2022-09-23 21:47:55 -0400

    conforming to solution...

--- day_39 (flight deals)/main.py
+++ day_39 (flight deals)/main.py
@@ -1,5 +1,4 @@
 from data_manager import DataManager
-
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
 
@@ -8,8 +7,7 @@ if sheet_data[0]["iataCode"] == "":
     flight_search = FlightSearch()
     for row in sheet_data:
         row["iataCode"] = flight_search.get_destination_code(row["city"])
-    print(f"sheet_data:\n {sheet_data}")
+    print(sheet_data)
 
     data_manager.destination_data = sheet_data
     data_manager.update_destination_codes()
-    
