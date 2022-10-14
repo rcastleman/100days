64db9dac41c533d9ce5ddced80fbae7edf0fa2e9
Randy Castleman <rcastleman@gmail.com>
2022-10-13 21:49:27 -0400

    soup

--- 45 (beautiful soup)/main.py
+++ 45 (beautiful soup)/main.py
@@ -3,4 +3,12 @@ from bs4 import BeautifulSoup
 with open("website.html") as f:
     contents = f.read()
 
-soup = BeautifulSoup(contents)
+soup = BeautifulSoup(contents,"html.parser")
+
+# print(soup.title)
+# print(soup.title.name)
+# print(soup.title.string)
+
+# print(soup.prettify())
+
+print(soup.a)
