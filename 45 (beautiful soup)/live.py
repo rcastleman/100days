2ae40fb653873bb28a3355c8894a521a78b022ba
Randy Castleman <rcastleman@gmail.com>
2022-10-15 21:28:05 -0400

    parser not working

--- 45 (beautiful soup)/live.py
+++ 45 (beautiful soup)/live.py
@@ -1,14 +1,13 @@
 from bs4 import BeautifulSoup
-
 import requests
+import lxml
+
+PARSER = "lxml"
+# PARSER = "html.parser"
 
 response = requests.get("https://news.ycombinator.com/")
+yc_web_page = response.text
 
-yc_website = response.text
-
-soup = BeautifulSoup(yc_website,"html.parser")
-
+soup = BeautifulSoup(yc_web_page,PARSER)
 article_tag = soup.find(name="a",class_="storylink")
-
 print(article_tag)
-
