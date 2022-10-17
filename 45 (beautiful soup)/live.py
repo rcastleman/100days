558c575beb1b1756b02bfec271c2a930641ffc16
Randy Castleman <rcastleman@gmail.com>
2022-10-16 20:34:21 -0400

    soup crap

--- 45 (beautiful soup)/live.py
+++ 45 (beautiful soup)/live.py
@@ -2,12 +2,14 @@ from bs4 import BeautifulSoup
 import requests
 import lxml
 
-PARSER = "lxml"
-# PARSER = "html.parser"
+# PARSER = "lxml"
+PARSER = "html.parser"
 
 response = requests.get("https://news.ycombinator.com/")
 yc_web_page = response.text
 
 soup = BeautifulSoup(yc_web_page,PARSER)
-article_tag = soup.find(name="a",class_="storylink")
+heading = soup.find(name="h1", id="name")
+article_tag = soup.find(name="a")
+# print(soup.prettify())
 print(article_tag)
