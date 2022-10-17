f02a5f8996254cc72ec329485d6d40aafe144e22
Randy Castleman <rcastleman@gmail.com>
2022-10-17 19:34:02 -0400

    movies

--- 45 (movie scraping)/main.py
+++ 45 (movie scraping)/main.py
@@ -1,8 +1,9 @@
+from urllib import request
 import requests
 from bs4 import BeautifulSoup
 
 URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
-# Write your code below this line 👇
 
+response = requests.get(URL)
 
