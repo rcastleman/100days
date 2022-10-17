80e4c63f7d87c4a065a1c00e531c44f332e3ab62
Randy Castleman <rcastleman@gmail.com>
2022-10-16 21:37:28 -0400

    for loop soup

--- 45 (beautiful soup)/live.py
+++ 45 (beautiful soup)/live.py
@@ -9,11 +9,21 @@ response = requests.get("https://news.ycombinator.com/")
 yc_web_page = response.text
 
 soup = BeautifulSoup(yc_web_page,PARSER)
-heading = soup.find(name="h1", id="name")
-article_tag = soup.find(name="span",class_="titleline")
-article_text = article_tag.getText()
-article_link = article_tag.a.get("href")
-article_upvote = soup.find(name="span",class_="score").getText()
+# heading = soup.find(name="h1", id="name")
+articles = soup.find_all(name="span",class_="titleline")
+
+article_texts = []
+article_links = []
+
+for article_tag in articles:
+
+    article_text = article_tag.getText()
+    article_texts.append(article_text)
+    
+    article_link = article_tag.a.get("href")
+    article_links.append(article_link)
+
+article_upvote = soup.find_all(name="span",class_="score").getText()
 # print(soup.title)
 # print(soup.prettify())
 # print(article_tag)
