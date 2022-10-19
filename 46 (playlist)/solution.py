c979138c6a44c7f9112dbd6f7909cd2bb42fac4d
Randy Castleman <rcastleman@gmail.com>
2022-10-18 22:24:19 -0400

    spotify playlist created

--- 46 (playlist)/solution.py
+++ 46 (playlist)/solution.py
@@ -7,6 +7,9 @@ import os
 
 load_dotenv()
 
+# emulating this solution:
+# https://gist.github.com/angelabauer/e6087a48f9d1a87d4ec15fc29830892b?permalink_comment_id=4250715#gistcomment-4250715
+
 #----- GET TITLES -------- #
 
 date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
@@ -31,3 +34,13 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
                                                scope="playlist-modify-private",
                                              ))
                                             
+
+results = sp.current_user()
+USER_ID = results['id']
+uris = [sp.search(title)['tracks']['items'][0]['uri'] for title in titles]
+
+#------------------CREATING PLAYLIST---------------------------#
+
+PLAYLIST_ID = sp.user_playlist_create(user=USER_ID,public=False,name=f"{date} BillBoard-100")['id']
+
+sp.user_playlist_add_tracks(playlist_id=PLAYLIST_ID,tracks=uris,user=USER_ID)
--- 46 (playlist)/test.py
+++ 46 (playlist)/test.py
@@ -1,8 +1,7 @@
 import secrets
 from dotenv import load_dotenv
 load_dotenv()
-
-
 import os
+
 test = os.environ.get("SECRET")
 print(test)
