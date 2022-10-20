e595146fcd16dfbe6a058212c118fee6383e21ba
Randy Castleman <rcastleman@gmail.com>
2022-10-19 22:14:01 -0400

    headers

--- 47 (Amazon)/main.py
+++ 47 (Amazon)/main.py
@@ -3,3 +3,13 @@ from bs4 import BeautifulSoup
 
 URL = "https://www.amazon.com/Instant-Pot-Duo-Evo-Plus/dp/B07W55DDFB/ref=sr_1_1?qid=1597662463"
 
+User_Agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
+
+Accept_Language = "en-US,en;q=0.9,fr;q=0.8"
+
+HEADERS = {
+    User_Agent,
+    Accept_Language}
+
+response = requests.get(URL,headers = HEADERS)
+
