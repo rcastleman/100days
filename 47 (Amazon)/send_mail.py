d77debc26b2e53f1bdb12459c70aa81de0ffbfb6
Randy Castleman <rcastleman@gmail.com>
2022-10-28 21:46:48 -0400

    email

--- 47 (Amazon)/send_mail.py
+++ 47 (Amazon)/send_mail.py
@@ -1,9 +1,16 @@
 import smtplib
+import datetime as dt
 
 gmail_email = "rc.code.test@gmail.com"
 gmail_password =  "xxfgumnxsleullfm"
 gmail_SMTP = "smtp.gmail.com"
 send_to = "rcastleman@gmail.com"
+
+now = dt.datetime.now()
+hour = now.hour
+minute = now.minute
+
+email_body = f"Subject:test at {hour}:{minute} \n\nTest email body ...  "
 
 
 def send_email(body):
@@ -13,3 +20,4 @@ def send_email(body):
     server.login(gmail_email, gmail_password)
     server.sendmail(gmail_email,send_to,f"Subject:Happy Birthday!\n\n{body}")
     server.quit()
+
