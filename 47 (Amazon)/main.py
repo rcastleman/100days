f3b40eddf56225aa71597466833101e023fa0add
Randy Castleman <rcastleman@gmail.com>
2022-10-28 22:14:24 -0400

    ints and strings

--- 47 (Amazon)/main.py
+++ 47 (Amazon)/main.py
@@ -4,6 +4,9 @@ from bs4 import BeautifulSoup
 import lxml
 from send_mail import send_email
 
+
+#--------------- SCRAPING -------------------#
+
 URL = "https://www.amazon.com/Instant-Pot-6Qt-Plus-WiFi/dp/B08TMTJZ8L?ref_=ast_sto_dp"
 
 User_Agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
@@ -27,4 +30,18 @@ soup = BeautifulSoup(site_text,PARSER)
 data = soup.find(name="span", class_="a-offscreen")
 price = data.text
 
+today_price = float(price.strip('$'))
+
+# print(today_price)
+# print(type(today_price))
+
+
+#--------------- CHECK CURRENT PRICE -------------------#
+
+target_price = 200
+
+body = f"Subject:Price alert!\n\nThe current price is {price}, which is under your target price of {target_price}."
+
+if price < target_price:
+    send_email(body)
 
