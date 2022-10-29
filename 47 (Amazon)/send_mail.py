5533af8dfc63cd5f1f639dd1859cae409c4b878c
Randy Castleman <rcastleman@gmail.com>
2022-10-28 21:56:41 -0400

    email+

--- 47 (Amazon)/send_mail.py
+++ 47 (Amazon)/send_mail.py
@@ -10,14 +10,14 @@ now = dt.datetime.now()
 hour = now.hour
 minute = now.minute
 
-email_body = f"Subject:test at {hour}:{minute} \n\nTest email body ...  "
-
+body = f"Subject:test at {hour}:{minute} \n\nTEXT IN THE BODY HERE"
 
 def send_email(body):
     server = smtplib.SMTP()
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
     server.login(gmail_email, gmail_password)
-    server.sendmail(gmail_email,send_to,f"Subject:Happy Birthday!\n\n{body}")
+    server.sendmail(gmail_email,send_to,body)
     server.quit()
 
+# send_email(body)
