a7e93b314c3240b69dd8590d7542ad3d08859436
Randy Castleman <rcastleman@gmail.com>
2022-10-29 22:35:29 -0400

    selenium deprecation fixes

--- 48 (Selenium)/main.py
+++ 48 (Selenium)/main.py
@@ -1,6 +1,11 @@
-import selenium
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
-chrome_driver_path = "/Users/randycastleman/Dropbox/Mac/Documents/local_code/chrome/chromedriver"
 
-driver = webdriver.Chrome(executable_path=chrome_driver_path)
+# chrome_driver_path = "/Users/randycastleman/Dropbox/Mac/Documents/local_code/chrome/chromedriver"
+# driver = webdriver.Chrome(executable_path=chrome_driver_path)
+# got DEPRECATION error message, so the updated method (per https://stackoverflow.com/questions/64717302/deprecationwarning-executable-path-has-been-deprecated-selenium-python) is:
+ 
+s = Service("/Users/randycastleman/Dropbox/Mac/Documents/local_code/chrome/chromedriver")
+driver = webdriver.Chrome(service = s)
+driver.get("https://amazon.com")
