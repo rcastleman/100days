b8961b39130b240dfd49f51a03c6660c7e8abe11
Randy Castleman <rcastleman@gmail.com>
2022-10-29 22:22:30 -0400

    selenium installed

--- 48 (Selenium)/main.py
+++ 48 (Selenium)/main.py
@@ -1,1 +1,6 @@
-chrome_driver_path = "/Users/randycastleman/Dropbox/Mac/Documents/local_code/chrome"
+import selenium
+from selenium import webdriver
+
+chrome_driver_path = "/Users/randycastleman/Dropbox/Mac/Documents/local_code/chrome/chromedriver"
+
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
