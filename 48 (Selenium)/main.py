2733a8e5646c6d4bf9f36d3d0baf69fa74eb57b7
Randy Castleman <rcastleman@gmail.com>
2022-10-30 22:30:32 -0400

    selenium no work

--- 48 (Selenium)/main.py
+++ 48 (Selenium)/main.py
@@ -9,15 +9,16 @@ URL = "https://www.amazon.com/Instant-Pot-6Qt-Plus-WiFi/dp/B08TMTJZ8L?ref_=ast_s
 # driver = webdriver.Chrome(executable_path=chrome_driver_path)
 # got DEPRECATION error message, so the updated method (per https://stackoverflow.com/questions/64717302/deprecationwarning-executable-path-has-been-deprecated-selenium-python) is:
 
-x_path = "//*[@id='corePriceDisplay_desktop_feature_div']/div[1]/span/span[1]"
+x_path = '//*[@id="corePriceDisplay_desktop_feature_div"]/div[1]/span/span[1]'
 class_name = "a-offscreen"
+selector = "#corePriceDisplay_desktop_feature_div > div.a-section.a-spacing-none.aok-align-center > span > span.a-offscreen"
  
 s = Service("/Users/randycastleman/Dropbox/Mac/Documents/local_code/chrome/chromedriver")
 driver = webdriver.Chrome(service = s)
 driver.get(URL)
-# price = driver.find_element_by_class_name("a-offscreen")
-price = driver.find_element(By.CLASS_NAME,class_name)
+# price = driver.find_element(By.CLASS_NAME,class_name)
 # price = driver.find_element(By.XPATH,x_path)
+price = driver.find_element(By.CSS_SELECTOR,selector)
 print(price.text)
 # driver.close() #closes a tab
 driver.quit() #closes the entire browser
