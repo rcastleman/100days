cabc11d7c16fd392ca1c0ef47c27753d8df5f9d0
Randy Castleman <rcastleman@gmail.com>
2022-10-31 22:34:08 -0400

    scraping looping

--- 48 (Selenium)/scrape.py
+++ 48 (Selenium)/scrape.py
@@ -8,23 +8,28 @@ from selenium.webdriver.common.by import By
 
 URL = "https://www.python.org/"
 
-x_path = '//*[@id="bylineInfo"]'
-class_name = "a-offscreen"
-selector = "#corePriceDisplay_desktop_feature_div > div.a-section.a-spacing-none.aok-align-center > span > span.a-offscreen"
+x_path = '//*[@id="content"]/div/section/div[3]/div[2]/div/ul'
+class_name = None
+selector = None
  
 s = Service("/Users/randycastleman/Dropbox/Mac/Documents/local_code/chrome/chromedriver")
 driver = webdriver.Chrome(service = s)
 driver.get(URL)
 # events = driver.find_elements(By.CLASS_NAME,class_name)
-events = driver.find_elements(By.XPATH,x_path)
+# events = driver.find_elements(By.XPATH,x_path)
 # events = driver.find_elements(By.CSS_SELECTOR,selector)
-print(events.text)
+
+events = []
+for item in driver.find_elements(By.XPATH,x_path):
+    date = item.find_element(By.CLASS_NAME,'say-no-more').text
+    event = item.find_element(By.CSS_SELECTOR,'a').text
+    events.append({'date': date, 'event name': event})
+
+print(events)
 driver.quit() #closes the entire browser
 
 #-------------- BUILD DICTIONARY ---------------#
 
 # {key: value for (key, value) in iterable}
 
-
-
-event_dict = {k:v for (k,v) in iterable}
+# event_dict = {k:v for (k,v) in iterable}
