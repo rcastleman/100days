69bda3fdf8f692ca7824b4a2076823a1fd9869e8
Randy Castleman <rcastleman@gmail.com>
2022-11-01 21:06:06 -0400

    wiki scraping...

--- 48 (Selenium)/wiki.py
+++ 48 (Selenium)/wiki.py
@@ -4,11 +4,12 @@ from selenium.webdriver.common.by import By
 
 URL = "https://en.wikipedia.org/wiki/Main_Page"
 
-x_path = None
-class_name = None
+x_path = '//*[@id="articlecount"]/a[1]'
  
 s = Service("/Users/randycastleman/Dropbox/Mac/Documents/local_code/chrome/chromedriver")
 driver = webdriver.Chrome(service = s)
 driver.get(URL)
 # events = driver.find_elements(By.CLASS_NAME,class_name)
-# events = driver.find_elements(By.XPATH,x_path)
+stat = driver.find_elements(By.XPATH,x_path)
+
+print(stat.text)
