9745e6559699fdb00513a5b3a321f66f5c046f07
Randy Castleman <rcastleman@gmail.com>
2022-11-01 22:23:50 -0400

    cookie start

---
+++ 48 (Selenium)/cookie.py
@@ -0,0 +1,16 @@
+from pyexpat.errors import XML_ERROR_PARTIAL_CHAR
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+URL = None
+
+s = Service("/Users/randycastleman/Dropbox/Mac/Documents/local_code/chrome/chromedriver")
+driver = webdriver.Chrome(service = s)
+driver.get(URL)
+
+
+button.click()
+
+driver.quit()
