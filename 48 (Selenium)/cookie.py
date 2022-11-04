429e58a3f1aaaf338125dd47afe5f5435d3cb16c
Randy Castleman <rcastleman@gmail.com>
2022-11-03 23:01:22 -0400

    cookie

--- 48 (Selenium)/cookie.py
+++ 48 (Selenium)/cookie.py
@@ -4,12 +4,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
-URL = None
+URL = 'http://orteil.dashnet.org/experiments/cookie/'
 
 s = Service("/Users/randycastleman/Dropbox/Mac/Documents/local_code/chrome/chromedriver")
 driver = webdriver.Chrome(service = s)
 driver.get(URL)
 
+cookie = driver.find_element_by_id("cookie")
 
 button.click()
 
