d32117513fdfb051e8b3375c392a85b34844b29b
Randy Castleman <rcastleman@gmail.com>
2022-11-03 22:59:43 -0400

    selenium cookies

--- 48 (Selenium)/cookie.py
+++ 48 (Selenium)/cookie.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import time
 
 URL = None
 
