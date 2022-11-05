edeb22f1c1ed09263044b6d79e67c7dba312c248
Randy Castleman <rcastleman@gmail.com>
2022-11-04 21:39:10 -0400

    cobbling together solution

--- 48 (Selenium)/cookie.py
+++ 48 (Selenium)/cookie.py
@@ -12,10 +12,22 @@ driver.get(URL)
 
 cookie = driver.find_element_by_id("cookie")
 
-
 items = driver.find_elements_by_css_selector("#store div")
 item_ids = [item.get_attribute("id") for item in items]
 
-button.click()
+timeout = time.time() + 5
+five_min = time.time() + 60*5 # 5minutes
+
+while True:
+    cookie.click()
+
+    #Every 5 seconds:
+    # if time.time() > timeout:
+
+    #After 5 minutes stop the bot and check the cookies per second count.
+    if time.time() > five_min:
+        cookie_per_s = driver.find_element_by_id("cps").text
+        print(cookie_per_s)
+        break
 
 driver.quit()
