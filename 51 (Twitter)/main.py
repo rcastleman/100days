5d820b8f73728b2d46e66a2e40d589b8c9bb42be
Randy Castleman <rcastleman@gmail.com>
2022-11-08 22:42:55 -0500

    twitter

--- 51 (Twitter)/main.py
+++ 51 (Twitter)/main.py
@@ -14,16 +14,18 @@ PROMISED_UP = 250
 TWITTER_ID = os.environ.get("USER")
 TWITTER_PASS = os.environ.get("PASS")
 
-URL = 'https://twitter.com/home'
+TWITTER_URL = 'https://twitter.com/home'
+SPEEDTEST_URL = 'https://www.speedtest.net/'
+
 
 svce = Service("/Users/randycastleman/Dropbox/Mac/Documents/local_code/chrome/chromedriver")
 # driver = webdriver.Chrome(service = svce)
-driver.get(URL)
+# driver.get(URL)
 
 class InternetSpeedTwitterBot:
     def __init__(self):
         self.driver = webdriver.Chrome(service = svce)
-        self.up = 
+        self.up = driver.get(SPEEDTEST_URL)
         self.down = 
 
 bot = InternetSpeedTwitterBot()
