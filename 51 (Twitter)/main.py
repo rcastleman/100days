38f372c53fa66bc5d7ca8caa6f14ad2045b7a978
Randy Castleman <rcastleman@gmail.com>
2022-11-08 22:28:59 -0500

    twitter commence

---
+++ 51 (Twitter)/main.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
