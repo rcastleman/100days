5a0dba725380072ba5350061ff4bcaac432cec84
Randy Castleman <rcastleman@gmail.com>
2022-11-10 22:08:49 -0500

    twitterbot

--- 51 (Twitter)/main.py
+++ 51 (Twitter)/main.py
@@ -34,8 +34,3 @@ class InternetSpeedTwitterBot:
         self.down = driver.find_element(By.XPATH,'//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span')
     
 bot = InternetSpeedTwitterBot()
-
-print(bot.getDown)
-print(bot.getDown)
-print(bot.getDown)
-
