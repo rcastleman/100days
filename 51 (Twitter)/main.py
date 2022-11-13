4b7ec3f4095231e08bf30bb876f6e27910615fbb
Randy Castleman <rcastleman@gmail.com>
2022-11-12 20:14:35 -0500

    twitterbot

--- 51 (Twitter)/main.py
+++ 51 (Twitter)/main.py
@@ -18,7 +18,7 @@ TWITTER_URL = 'https://twitter.com/home'
 SPEEDTEST_URL = 'https://www.speedtest.net/'
 go_button = '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a/span[4]'
 down_readout = '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span'
-up_readout = ''
+up_readout = '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span'
 
 svce = Service("/Users/randycastleman/Dropbox/Mac/Documents/local_code/chrome/chromedriver")
 
@@ -31,12 +31,13 @@ class InternetSpeedTwitterBot:
     def get_internet_speed(self):
         self.driver.get(SPEEDTEST_URL)
         button = self.driver.find_element(By.XPATH,go_button)
-        time.sleep(15)
+        time.sleep(30)
         button.click()
         time.sleep(30)
-        # self.up = self.driver.find_element(By.XPATH,up_readout).text
+        self.up = self.driver.find_element(By.XPATH,up_readout).text
         self.down = self.driver.find_element(By.XPATH,down_readout).text
         print(self.down)
+        print(self.up)
     
     def tweet_at_provider(self):
         pass
