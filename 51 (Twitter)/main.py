cb9842f171c7f81a664dba2eddb0a962c0e7e8cc
Randy Castleman <rcastleman@gmail.com>
2022-11-12 20:56:00 -0500

    still no upload...

--- 51 (Twitter)/main.py
+++ 51 (Twitter)/main.py
@@ -20,7 +20,8 @@ go_button = '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a/
 down_readout = '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span'
 up_readout = '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span'
 svce = Service("/Users/randycastleman/Dropbox/Mac/Documents/local_code/chrome/chromedriver")
-close_button = '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[8]/div/a/svg/use'
+# close_button = '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[8]/div/a/svg/use'
+close_button = '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[8]/div/div/div[2]/a'
 
 class InternetSpeedTwitterBot:
     def __init__(self):
@@ -30,14 +31,13 @@ class InternetSpeedTwitterBot:
 
     def get_internet_speed(self):
         self.driver.get(SPEEDTEST_URL)
-        button = self.driver.find_element(By.XPATH,go_button)
+        time.sleep(15)
+        self.driver.find_element(By.XPATH,go_button).click()
         time.sleep(30)
-        button.click()
-        time.sleep(30)
-        self.driver.find_element(By.XPATH,close_button).click()
+        # self.driver.find_element(By.XPATH,close_button).click()
         self.down = self.driver.find_element(By.XPATH,down_readout).text
         self.up = self.driver.find_element(By.XPATH,up_readout).text
-        print(f"Download: {self.down} & Upload: {self.up}")
+        print(f"Download: {self.down}\nUpload: {self.up}")
 
     def tweet_at_provider(self):
         pass
