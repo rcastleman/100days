e50643c32dc5b3f8ed4bf181befce719ce5a106e
Randy Castleman <rcastleman@gmail.com>
2022-11-12 19:41:13 -0500

    twitterbot

--- 51 (Twitter)/main.py
+++ 51 (Twitter)/main.py
@@ -18,6 +18,7 @@ TWITTER_URL = 'https://twitter.com/home'
 SPEEDTEST_URL = 'https://www.speedtest.net/'
 speed_down = '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a/span[4]'
 
+
 svce = Service("/Users/randycastleman/Dropbox/Mac/Documents/local_code/chrome/chromedriver")
 
 class InternetSpeedTwitterBot:
@@ -26,13 +27,17 @@ class InternetSpeedTwitterBot:
         self.up = 0
         self.down = 0
 
-    def get_internet_speed():
-        driver = webdriver.Chrome(service = svce)
-        driver.get(SPEEDTEST_URL)
-        driver.find_element(By.XPATH,speed_down).click()
+    def get_internet_speed(self):
+        self.driver.get(SPEEDTEST_URL)
+        button = self.driver.find_element(By.XPATH,speed_down)
+        button.click()
     
-    def tweet_at_provider():
+    def tweet_at_provider(self):
         pass
 
+    def test(self):
+        return 'bot is working'
+
 bot = InternetSpeedTwitterBot()
-bot.get_internet_speed()
+
+# print(bot.test())
