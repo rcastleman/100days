d85a9de9f3a8aadeeccc4214e7cc34cb59c06e8e
Randy Castleman <rcastleman@gmail.com>
2022-11-13 22:06:37 -0500

    works! even without alert handling!

--- 51 (Twitter)/main.py
+++ 51 (Twitter)/main.py
@@ -31,11 +31,11 @@ class InternetSpeedTwitterBot:
 
     def get_internet_speed(self):
         self.driver.get(SPEEDTEST_URL)
-        time.sleep(15)
+        # time.sleep(15)
         self.driver.find_element(By.XPATH,go_button).click()
-        time.sleep(30)
-        alert_obj = self.driver.switch_to.alert
-        alert_obj.dismiss()
+        time.sleep(60)
+        # alert_obj = self.driver.switch_to.alert
+        # alert_obj.accept()
         # self.driver.find_element(By.XPATH,close_button).click()
         self.down = self.driver.find_element(By.XPATH,down_readout).text
         self.up = self.driver.find_element(By.XPATH,up_readout).text
