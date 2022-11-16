57f9934e098dad8437255b7653bd2f4e9887f491
Randy Castleman <rcastleman@gmail.com>
2022-11-16 16:57:55 -0500

    find and follow

--- 52 (Instagram)/main.py
+++ 52 (Instagram)/main.py
@@ -16,10 +16,6 @@ TARGET = 'paulsoninstitute/'
 
 svce = Service("/Users/randycastleman/Dropbox/Mac/Documents/local_code/chrome/chromedriver")
 
-class newClass:
-    def __init__(self):
-        self.driver = webdriver.Chrome(service = svce)
-
 USERNAME = os.environ.get('USER')
 PASSWORD = os.environ.get('PASS')
 
