4728e1d21ec2a7abce8b3069ad9afc106c26f24f
Randy Castleman <rcastleman@gmail.com>
2022-11-16 16:57:28 -0500

    find and follow...

--- 52 (Instagram)/main.py
+++ 52 (Instagram)/main.py
@@ -6,6 +6,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import ElementClickInterceptedException
+
 
 load_dotenv()
 
@@ -54,6 +56,16 @@ class InstaFollower():
         for i in range(10):
             self.driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", modal)
             time.sleep(2)
+
+    def follow(self):
+        all_buttons = self.driver.find_elements_by(By.CSS_SELECTOR,"li button")
+        for button in all_buttons:
+            try:
+                button.click()
+                time.sleep(1)
+            except ElementClickInterceptedException:
+                cancel_button = self.driver.find_element_by(By.XPATH,'/html/body/div[5]/div/div/div/div[3]/button[2]')
+                cancel_button.click()
 
 bot = InstaFollower()
 bot.login()
