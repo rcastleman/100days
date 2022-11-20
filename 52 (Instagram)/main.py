be945b7d628e2a2cb8de015d34a85dfe530fae52
Randy Castleman <rcastleman@gmail.com>
2022-11-19 20:51:03 -0500

    damned follower button ...

--- 52 (Instagram)/main.py
+++ 52 (Instagram)/main.py
@@ -19,7 +19,8 @@ svce = Service("/Users/randycastleman/Dropbox/Mac/Documents/local_code/chrome/ch
 USERNAME = os.environ.get('USER')
 PASSWORD = os.environ.get('PASS')
 
-FOLLOWERS_BUTTON = "//a[contains(@href, '/following')]"
+# FOLLOWERS_BUTTON = "//a[contains(@href, '/following')]"
+FOLLOWERS_BUTTON = '//*[@id="mount_0_0_WH"]/div/div/div/div[1]/div/div/div/div[1]/div[2]/div[2]/section/main/div/ul/li[2]/a'
 
 class InstaFollower():
     def __init__(self):
