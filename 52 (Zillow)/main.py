3b4de5039422e3e83412959bdbf567484414d403
Randy Castleman <rcastleman@gmail.com>
2022-11-25 16:26:54 -0500

    zillow

--- 52 (Zillow)/main.py
+++ 52 (Zillow)/main.py
@@ -1,3 +1,17 @@
+from urllib import request
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3Anull%2C%22mapBounds%22%3A%7B%22west%22%3A-122.56276167822266%2C%22east%22%3A-122.30389632177734%2C%22south%22%3A37.69261345230467%2C%22north%22%3A37.857877098316834%7D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22pmf%22%3A%7B%22value%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A12%7D'
+
+response = requests.get(URL)
+
+website_html = response.text
+
+soup = BeautifulSoup(website_html,"html.parser")
+
+print(soup.prettify())
+
 import os
 import time
 
@@ -18,3 +32,7 @@ USERNAME = os.environ.get('USER')
 PASSWORD = os.environ.get('PASS')
 
 FORM = 'https://forms.gle/WZPiFN9neKV8Gegh8'
+
+
+#--------- SCRAPE ALL LISTINGS ----------- #
+
