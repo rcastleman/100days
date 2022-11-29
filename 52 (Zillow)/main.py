66c15d200c10db316ab08676ec81d2bd858c93a1
Randy Castleman <rcastleman@gmail.com>
2022-11-28 21:48:32 -0500

    zilloooo

--- 52 (Zillow)/main.py
+++ 52 (Zillow)/main.py
@@ -21,9 +21,7 @@ load_dotenv()
 URL = 'https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3Anull%2C%22mapBounds%22%3A%7B%22west%22%3A-122.56276167822266%2C%22east%22%3A-122.30389632177734%2C%22south%22%3A37.69261345230467%2C%22north%22%3A37.857877098316834%7D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22pmf%22%3A%7B%22value%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A12%7D'
 
 response = requests.get(URL)
-
 site_data = response.text
-
 soup = BeautifulSoup(site_data,"html.parser")
 
 LISTINGS_XPATH = '//*[@id="grid-search-results"]/ul'
@@ -31,8 +29,12 @@ LISTINGS_SELECTOR = '#grid-search-results > ul'
 FIRST_LISTING = '#grid-search-results > ul > li:nth-child(1)'
 
 # all_link_elements = soup.select(".list-card-top a") # solution's address, which doesn't work
-all_link_elements = soup.findAll('.grid-search-result ul')
-print(f'all_link_elements = {all_link_elements}')
+all_link_elements = soup.findAll(FIRST_LISTING)
+
+parent = soup.find("body").find("ul")
+print(parent)
+
+# print(f'all_link_elements = {all_link_elements}')
 
 all_links = []
 for link in all_link_elements:
@@ -45,8 +47,6 @@ for link in all_link_elements:
 
 # print(all_links)
 # print(soup.prettify())
-
-
 
 LIST_ID = "search-page-list-container"
 
