d66a63ba3e8b5f9e1f04fd1f380a942f26691f9d
Randy Castleman <rcastleman@gmail.com>
2022-11-28 22:23:35 -0500

    headers

--- 52 (Zillow)/main.py
+++ 52 (Zillow)/main.py
@@ -17,10 +17,14 @@ load_dotenv()
 
 
 #--------- SCRAPE ALL LISTINGS ----------- #
+header = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
+    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
+}
 
 URL = 'https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3Anull%2C%22mapBounds%22%3A%7B%22west%22%3A-122.56276167822266%2C%22east%22%3A-122.30389632177734%2C%22south%22%3A37.69261345230467%2C%22north%22%3A37.857877098316834%7D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22pmf%22%3A%7B%22value%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A12%7D'
 
-response = requests.get(URL)
+response = requests.get(UR,headers=header)
 site_data = response.text
 soup = BeautifulSoup(site_data,"html.parser")
 
@@ -28,12 +32,9 @@ LISTINGS_XPATH = '//*[@id="grid-search-results"]/ul'
 LISTINGS_SELECTOR = '#grid-search-results > ul'
 FIRST_LISTING = '#grid-search-results > ul > li:nth-child(1)'
 
+
 # all_link_elements = soup.select(".list-card-top a") # solution's address, which doesn't work
-all_link_elements = soup.findAll(FIRST_LISTING)
-
-parent = soup.find("body").find("ul")
-print(parent)
-
+all_link_elements = soup.findAll(LISTINGS_SELECTOR)
 # print(f'all_link_elements = {all_link_elements}')
 
 all_links = []
@@ -45,8 +46,7 @@ for link in all_link_elements:
     else:
         all_links.append(href)
 
-# print(all_links)
-# print(soup.prettify())
+print(soup.prettify())
 
 LIST_ID = "search-page-list-container"
 
