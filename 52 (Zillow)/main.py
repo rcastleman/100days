04a9e4e0b8cbc9b27ae54e75d439e28e385dce06
Randy Castleman <rcastleman@gmail.com>
2022-11-28 21:24:58 -0500

    zillow

--- 52 (Zillow)/main.py
+++ 52 (Zillow)/main.py
@@ -1,7 +1,22 @@
+#--------- DEPENDENCIES ---------------- #
 from bs4 import BeautifulSoup
 import requests
 from selenium import webdriver
 import time
+import os
+import time
+
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import ElementClickInterceptedException
+
+load_dotenv()
+
+
+#--------- SCRAPE ALL LISTINGS ----------- #
 
 URL = 'https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3Anull%2C%22mapBounds%22%3A%7B%22west%22%3A-122.56276167822266%2C%22east%22%3A-122.30389632177734%2C%22south%22%3A37.69261345230467%2C%22north%22%3A37.857877098316834%7D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22pmf%22%3A%7B%22value%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A12%7D'
 
@@ -31,17 +46,7 @@ for link in all_link_elements:
 # print(all_links)
 # print(soup.prettify())
 
-import os
-import time
 
-from dotenv import load_dotenv
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import ElementClickInterceptedException
-
-load_dotenv()
 
 LIST_ID = "search-page-list-container"
 
@@ -53,5 +58,4 @@ PASSWORD = os.environ.get('PASS')
 FORM = 'https://forms.gle/WZPiFN9neKV8Gegh8'
 
 
-#--------- SCRAPE ALL LISTINGS ----------- #
 
