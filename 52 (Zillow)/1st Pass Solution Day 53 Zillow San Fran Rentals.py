726aab97d165ff25f0e42a7989fde9551f244e9a
Randy Castleman <rcastleman@gmail.com>
2022-11-29 22:06:18 -0500

    zillow with captcha

--- 52 (Zillow)/1st Pass Solution Day 53 Zillow San Fran Rentals.py
+++ 52 (Zillow)/1st Pass Solution Day 53 Zillow San Fran Rentals.py
@@ -24,6 +24,8 @@ for link in all_link_elements:
     else:
         all_links.append(href)
 
+# print(f'all_link_elements = {all_link_elements}')
+
 all_address_elements = soup.select(".list-card-info address")
 all_addresses = [address.get_text().split(" | ")[-1] for address in all_address_elements]
 
@@ -31,22 +33,23 @@ all_price_elements = soup.select(".list-card-details li")
 all_prices = [price.get_text().split("+")[0] for price in all_price_elements if "$" in price.text]
 
 
-chrome_driver_path = YOUR_PATH_TO_THE_CHROME_DRIVER
-driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-for n in range(len(all_links)):
-    driver.get(YOUR_GOOGLE_FORM_LINK)
+# chrome_driver_path = YOUR_PATH_TO_THE_CHROME_DRIVER
+# driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+# for n in range(len(all_links)):
+#     driver.get(YOUR_GOOGLE_FORM_LINK)
     
-    time.sleep(2)
-    address = driver.find_element_by_xpath(
-        '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
-    price = driver.find_element_by_xpath(
-        '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
-    link = driver.find_element_by_xpath(
-        '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
-    submit_button = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div')
+#     time.sleep(2)
+#     address = driver.find_element_by_xpath(
+#         '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+#     price = driver.find_element_by_xpath(
+#         '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+#     link = driver.find_element_by_xpath(
+#         '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+#     submit_button = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div')
 
-    address.send_keys(all_addresses[n])
-    price.send_keys(all_prices[n])
-    link.send_keys(all_links[n])
-    submit_button.click()
+#     address.send_keys(all_addresses[n])
+#     price.send_keys(all_prices[n])
+#     link.send_keys(all_links[n])
+#     submit_button.click()
