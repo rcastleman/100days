88a67469fdbb521d0847305f2305edb54cb88fcf
Randy Castleman <rcastleman@gmail.com>
2022-12-03 21:47:11 -0500

    still not working...

--- 52 (Zillow)/Solution Day 53 Zillow San Fran Rentals Debugged.py
+++ 52 (Zillow)/Solution Day 53 Zillow San Fran Rentals Debugged.py
@@ -46,21 +46,18 @@ for element in all_price_elements:
 
 # Create Spreadsheet using Google Form
 # Substitute your own path here 👇
-chrome_driver_path = YOUR_PATH_HERE
-driver = webdriver.Chrome(executable_path=chrome_driver_path)
+chrome_driver_path = '/Users/randycastleman/Dropbox/Mac/Documents/local_code/chrome/chromedriver'
+driver = webdriver.Chrome(service=chrome_driver_path)
 
 for n in range(len(all_links)):
     # Substitute your own Google Form URL here 👇
-    driver.get(URL_TO_YOUR_GOOGLE_FORM)
+    driver.get('https://forms.gle/WZPiFN9neKV8Gegh8')
 
     time.sleep(2)
-    address = driver.find_element_by_xpath(
-        '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
-    price = driver.find_element_by_xpath(
-        '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
-    link = driver.find_element_by_xpath(
-        '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
-    submit_button = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div')
+    address = driver.find_element(By.XPATH,'//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    price = driver.find_element(By.XPATH,'//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    link = driver.find_element(By.XPATH,'//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    submit_button = driver.find_element(By.XPATH,'//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div')
 
     address.send_keys(all_addresses[n])
     price.send_keys(all_prices[n])
