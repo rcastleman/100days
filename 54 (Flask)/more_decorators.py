b7a2d6abb9f0c597fde5a617b39b34d5719c11d7
Randy Castleman <rcastleman@gmail.com>
2022-12-08 22:04:30 -0500

    delays

--- 54 (Flask)/more_decorators.py
+++ 54 (Flask)/more_decorators.py
@@ -4,8 +4,16 @@ def delay(function):
     def wrapper_function():
         time.sleep(2)
         function()
+        time.sleep(2)
     return wrapper_function
 
-@delay
+# @delay
 def say_hello():
     print("Hello")
+
+@delay
+def say_bye():
+    print("bye")
+
+say_hello()
+say_bye()
