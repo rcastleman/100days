e40c6ce0a3d190bb8c5163b829bf1765e919a005
Randy Castleman <rcastleman@gmail.com>
2022-12-10 21:49:39 -0500

    flask day 2

--- 55 (Flask)/main.py
+++ 55 (Flask)/main.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Big World!'
+    return 'Hello, World!'
 
 @app.route('/bye')
 def say_bye():
