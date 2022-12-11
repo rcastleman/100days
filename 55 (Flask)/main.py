2412341f040533728d771233b6740bdc9725bdbc
Randy Castleman <rcastleman@gmail.com>
2022-12-10 22:49:10 -0500

    flask variables

--- 55 (Flask)/main.py
+++ 55 (Flask)/main.py
@@ -4,16 +4,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return '<h1> Hello, World!</h1>'
 
 @app.route('/bye')
 def say_bye():
     return 'bye'
 
 # @app.route("/username/<path:name>")
-# @app.route("/username/<name>/<int:number>")
+# @app.route("/username/<name>")
+@app.route("/username/<name>/<int:number>")
 def greet(name,number):
-    return f'Hello {name}, you are #{number}!'
+    return f'Hello {name}, you are {number} years old but you only look {number-30}!'
 
 if __name__ == "__main__":
     app.run(debug=True)
