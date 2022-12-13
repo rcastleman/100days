d6bf36ef1ff7e96c1a6de8765fda5535cd026381
Randy Castleman <rcastleman@gmail.com>
2022-12-12 22:10:39 -0500

    args and kwargs

--- 55 (Flask)/advanced_dec.py
+++ 55 (Flask)/advanced_dec.py
@@ -9,6 +9,12 @@ class User:
 def create_blog(user):
     print(f"This is {user.name}'s blog post")
 
+def is_authenticated_decorator(function):
+    def wrapper():
+        if user.is_loggedin = True:
+            function()
+    return wrapper
+
 Randy = User('Randy')
 
 create_blog(Randy)
