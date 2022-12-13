b9ebb01442e7145662366da71cf8ccbf5bf251e6
Randy Castleman <rcastleman@gmail.com>
2022-12-12 22:17:04 -0500

    args and kwargs OMG

--- 55 (Flask)/advanced_dec.py
+++ 55 (Flask)/advanced_dec.py
@@ -4,17 +4,18 @@ class User:
     def __init__(self,name):
         self.name = name
         self.is_logged_in = False
-    
 
+def is_authenticated_decorator(function):
+    def wrapper(*args,**kwargs):
+        if args[0].is_logged_in == True:
+            function(args[0])
+    return wrapper
+
+@is_authenticated_decorator
 def create_blog(user):
     print(f"This is {user.name}'s blog post")
 
-def is_authenticated_decorator(function):
-    def wrapper():
-        if user.is_loggedin = True:
-            function()
-    return wrapper
-
 Randy = User('Randy')
+Randy.is_logged_in = True
 
 create_blog(Randy)
