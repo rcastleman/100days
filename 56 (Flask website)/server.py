81d9e4c0444c981706adacb9cff617a4b440d98b
Randy Castleman <rcastleman@gmail.com>
2022-12-13 23:03:23 -0500

    templates

---
+++ 56 (Flask website)/server.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route('/bye')
+def say_bye():
+    return 'bye'
+
+if __name__ == "__main__":
+    app.run(debug=True)
