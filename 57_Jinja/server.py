086aa53d5afbb24399d1d197d7c0d94f0fe80190
Randy Castleman <rcastleman@gmail.com>
2022-12-16 21:28:26 -0500

    added copyright year

--- 57_Jinja/server.py
+++ 57_Jinja/server.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask import render_template
 import random
+import datetime
+year = datetime.date.today().year
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     random_number = random.randint(1,10)
-    return render_template("index.html",num=random_number)
+    return render_template("index.html",num=random_number,year=year)
  
 if __name__ == "__main__":
     app.run(debug=True)
