e861d385d189ff526524f7c397df7c45a12cf838
Randy Castleman <rcastleman@gmail.com>
2022-12-16 20:55:41 -0500

    minor change

--- 57_Jinja/server.py
+++ 57_Jinja/server.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return render_template('index.html')
+    # return render_template('index.html')
+    return "Hello World"
 
 if __name__ == "__main__":
     app.run(debug=True)
