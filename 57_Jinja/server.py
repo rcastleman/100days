38d56632b286a9dc1e1e119ef43a8f475b9f54dd
Randy Castleman <rcastleman@gmail.com>
2022-12-16 21:06:44 -0500

    randint

--- 57_Jinja/server.py
+++ 57_Jinja/server.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from flask import render_template
+import random
 
 app = Flask(__name__)
 
+app.route('/')
+def random_function():
+    random_number = random.randint(1,10)
+    return render_template("index.html",num=random_number)
+
+
 @app.route('/')
 def root():
-    # return render_template('index.html')
-    return "Hello World"
+    return render_template('index.html')
+    # return "Hello World"
 
 if __name__ == "__main__":
     app.run(debug=True)
