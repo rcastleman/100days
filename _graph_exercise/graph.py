9427da275b414af6d1f03466964a5659aa49543b
Randy Castleman <rcastleman@gmail.com>
2022-12-17 19:32:48 -0500

    graphs exercise

---
+++ _graph_exercise/graph.py
@@ -0,0 +1,11 @@
+
+if __name__ == "__main__":
+    routes = [
+        ("Mumbai","Paris"),
+        ("Mumbai","Dubai"),
+        ("Paris","Dubai"),
+        ("Paris","New York"),
+        ("Dubai","New York"),
+        ("New York","Toronto"),
+    ]
+
