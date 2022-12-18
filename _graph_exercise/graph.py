49874dd9ea8b0961d8938ffd163166a7f563f7a4
Randy Castleman <rcastleman@gmail.com>
2022-12-17 19:42:39 -0500

    graph exercise

--- _graph_exercise/graph.py
+++ _graph_exercise/graph.py
@@ -1,9 +1,17 @@
+# from https://youtu.be/j0IYCyBdzfA
+# and https://github.com/codebasics/data-structures-algorithms-python/blob/master/data_structures/10_Graph/graph.py
+
 class Graph:
     def __init__(self,edges) -> None:
         self.edges = edges
         self.graph_dict = {}
         for start,end in self.edges:
-            
+            if start in self.graph_dict:
+                self.graph_dict[start].append(end)
+            else:
+                self.graph_dict[start] = [end]
+        print(f"Graph Dictionary: {self.graph_dict}")
+
 
 if __name__ == "__main__":
     routes = [
