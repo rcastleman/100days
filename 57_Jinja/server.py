d1a663d8a7d41a10ee3ac10c0366477dd142887b
Randy Castleman <rcastleman@gmail.com>
2022-12-19 21:54:51 -0500

    added blog text to API

--- 57_Jinja/server.py
+++ 57_Jinja/server.py
@@ -31,7 +31,7 @@ def guess(input_name):
  
 @app.route('/blog')
 def blog():
-    blog_url = 'https://api.npoint.io/9f34e72378d72554938c'
+    blog_url = 'https://api.npoint.io/1e222d179ee8b4e58a05'
     response = requests.get(blog_url)
     all_posts = response.json()
     return render_template("blog.html",posts = all_posts,year=year)
