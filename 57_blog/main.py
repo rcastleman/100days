2b387716fb8cbd4e6f022f4c3a03d4268017fa0b
Randy Castleman <rcastleman@gmail.com>
2022-12-22 22:52:38 -0500

    blog

--- 57_blog/main.py
+++ 57_blog/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import requests
 
 
 app = Flask(__name__)
@@ -6,6 +7,13 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template("index.html")
+
+def get_blog(num):
+    print(num)
+    blog_url = 'https://api.npoint.io/1e222d179ee8b4e58a05'
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("blog.html",post = all_posts)
 
 if __name__ == "__main__":
     app.run(debug=True)
