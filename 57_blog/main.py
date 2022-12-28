6e9a3c0f0da7dcfb551b42e23e28bce093d17ff3
Randy Castleman <rcastleman@gmail.com>
2022-12-27 22:05:39 -0500

    added post.hmtl and post.py

--- 57_blog/main.py
+++ 57_blog/main.py
@@ -1,16 +1,23 @@
 from flask import Flask, render_template
+from post import Post
 import requests
-
 
 app = Flask(__name__)
 
+blog_url = 'https://api.npoint.io/1e222d179ee8b4e58a05'
+response = requests.get(blog_url)
+all_posts = response.json()
+
 @app.route('/')
 def home():
-    # return render_template("index.html")
-    blog_url = 'https://api.npoint.io/1e222d179ee8b4e58a05'
-    response = requests.get(blog_url)
-    all_posts = response.json()
     return render_template("index.html",posts = all_posts)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    for blog_post in all_posts:
+        if blog_post.id == index:
+            requested_post = blog_post
+    return render_template("post.html",post = requested_post)
