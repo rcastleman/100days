ddd6bff4fe5619c695583a70d8538b5a0cad9fbb
Randy Castleman <rcastleman@gmail.com>
2022-12-29 20:32:35 -0500

    fixing...

--- 57_blog/main.py
+++ 57_blog/main.py
@@ -7,11 +7,11 @@ app = Flask(__name__)
 blog_url = 'https://api.npoint.io/1e222d179ee8b4e58a05'
 response = requests.get(blog_url)
 # all_posts = response.json()
-all_posts = "static/blog_text.txt"
+post_objects = "static/blog_text.txt"
 
 @app.route('/')
-def home():
-    return render_template("index.html")
+def get_all_posts():
+    return render_template("index.html", all_posts=post_objects)
 
 if __name__ == "__main__":
     app.run(debug=True)
